3d7ce763247e1c5f3baf085d132ea2d1e593dcc0
Adrian Kunz <clashsoft@hotmail.com>
2020-01-16 16:55:57 +0100

    POST assignment on submit

--- src/main/resources/org/fulib/webapp/static/assignment/create.js
+++ src/main/resources/org/fulib/webapp/static/assignment/create.js
@@ -1,3 +1,12 @@
+const titleInput = document.getElementById('titleInput');
+const authorInput = document.getElementById('authorInput');
+const emailInput = document.getElementById('emailInput');
+const deadlineDateInput = document.getElementById('deadlineDateInput');
+const deadlineTimeInput = document.getElementById('deadlineTimeInput');
+const descriptionInput = document.getElementById('descriptionInput');
+
+const assignmentLink = document.getElementById('link');
+
 const solutionInput = document.getElementById('solutionInput');
 const solutionInputCM = CodeMirror.fromTextArea(solutionInput, {
 	theme: 'idea',
@@ -23,4 +32,30 @@ function updateEditorTheme(theme = getTheme()) {
 	let editorTheme = theme === 'dark' ? 'darcula' : 'idea';
 	solutionInputCM.setOption('theme', editorTheme);
 	verificationInputCM.setOption('theme', editorTheme);
+}
+
+function submit() {
+	const data = {
+		title: titleInput.value,
+		author: authorInput.value,
+		email: emailInput.value,
+		deadline: new Date(deadlineDateInput.value + ' ' + deadlineTimeInput.value).toISOString(),
+		description: descriptionInput.value,
+		solution: solutionInputCM.getValue(),
+		tasks: [
+			{
+				description: '', // TODO input field
+				points: 100, // TODO input field
+				verification: verificationInputCM.getValue(),
+			},
+		],
+	};
+
+	api('POST', '/assignment', data, result => {
+		const id = result.id;
+		const link = `https://www.fulib.org/assignment/${id}`;
+		assignmentLink.href = link;
+		assignmentLink.innerText = link;
+		$('#successModal').modal('show');
+	});
 }
