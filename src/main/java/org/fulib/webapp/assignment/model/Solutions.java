9021275fdd37319d57bd7a1df4f440ea9077809c
Adrian Kunz <clashsoft@hotmail.com>
2020-01-18 14:12:32 +0100

    Fix null assignment in Solutions.create

--- src/main/java/org/fulib/webapp/assignment/model/Solutions.java
+++ src/main/java/org/fulib/webapp/assignment/model/Solutions.java
@@ -24,7 +24,7 @@ public class Solutions
 		// TODO check deadline
 
 		final String solutionID = UUID.randomUUID().toString();
-		final Solution solution = fromJson(solutionID, new JSONObject(request.body()));
+		final Solution solution = fromJson(solutionID, assignment, new JSONObject(request.body()));
 
 		Mongo.get().saveSolution(solution);
 
@@ -33,9 +33,10 @@ public class Solutions
 		return result.toString(2);
 	}
 
-	private static Solution fromJson(String id, JSONObject obj)
+	private static Solution fromJson(String id, Assignment assignment, JSONObject obj)
 	{
 		final Solution solution = new Solution(id);
+		solution.setAssignment(assignment);
 		solution.setName(obj.getString(Solution.PROPERTY_name));
 		solution.setStudentID(obj.getString(Solution.PROPERTY_studentID));
 		solution.setEmail(obj.getString(Solution.PROPERTY_email));
