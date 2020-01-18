50eb5b44c49012cc04c9b16e0e96941ee48fdc92
Adrian Kunz <clashsoft@hotmail.com>
2020-01-18 13:50:39 +0100

    Add Solution model class

---
+++ src/main/java/org/fulib/webapp/assignment/model/Solution.java
@@ -0,0 +1,88 @@
+package org.fulib.webapp.assignment.model;
+
+public class Solution
+{
+	// =============== Constants ===============
+
+	public static final String PROPERTY_id         = "id";
+	public static final String PROPERTY_assignment = "assignment";
+	public static final String PROPERTY_name       = "name";
+	public static final String PROPERTY_studentID  = "studentID";
+	public static final String PROPERTY_email      = "email";
+	public static final String PROPERTY_solution   = "solution";
+
+	// =============== Fields ===============
+
+	private final String id;
+
+	private Assignment assignment;
+
+	private String name;
+	private String studentID;
+	private String email;
+	private String solution;
+
+	// =============== Constructors ===============
+
+	public Solution(String id)
+	{
+		this.id = id;
+	}
+
+	// =============== Properties ===============
+
+	public String getID()
+	{
+		return this.id;
+	}
+
+	public Assignment getAssignment()
+	{
+		return this.assignment;
+	}
+
+	public void setAssignment(Assignment assignment)
+	{
+		this.assignment = assignment;
+	}
+
+	public String getName()
+	{
+		return this.name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getStudentID()
+	{
+		return this.studentID;
+	}
+
+	public void setStudentID(String studentID)
+	{
+		this.studentID = studentID;
+	}
+
+	public String getEmail()
+	{
+		return this.email;
+	}
+
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+
+	public String getSolution()
+	{
+		return this.solution;
+	}
+
+	public void setSolution(String solution)
+	{
+		this.solution = solution;
+	}
+}
