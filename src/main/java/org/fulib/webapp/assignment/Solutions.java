e29ccf5ded10481fcb227476fff51d3fba57a60c
Adrian Kunz <clashsoft@hotmail.com>
2020-01-25 13:31:11 +0100

    Reorder members in Solutions

--- src/main/java/org/fulib/webapp/assignment/Solutions.java
+++ src/main/java/org/fulib/webapp/assignment/Solutions.java
@@ -23,6 +23,8 @@ import java.util.List;
 
 public class Solutions
 {
+	// --------------- Submission ---------------
+
 	public static Object create(Request request, Response response)
 	{
 		final Instant timeStamp = Instant.now();
@@ -52,6 +54,120 @@ public class Solutions
 		result.put("timeStamp", timeStamp.toString());
 		return result.toString(2);
 	}
+
+	private static Solution fromJson(String id, JSONObject obj)
+	{
+		final Solution solution = new Solution(id);
+		solution.setName(obj.getString(Solution.PROPERTY_name));
+		solution.setStudentID(obj.getString(Solution.PROPERTY_studentID));
+		solution.setEmail(obj.getString(Solution.PROPERTY_email));
+		solution.setSolution(obj.getString(Solution.PROPERTY_solution));
+		// don't include token!
+		return solution;
+	}
+
+	// --------------- Read ---------------
+
+	public static Object get(Request request, Response response)
+	{
+		final String assignmentID = request.params("assignmentID");
+		final String solutionID = request.params("solutionID");
+
+		if (request.contentType() == null || !request.contentType().startsWith("application/json"))
+		{
+			response.redirect("/assignment/solution.html?id=" + assignmentID + "&solution=" + solutionID);
+			return "";
+		}
+
+		final Solution solution = Mongo.get().getSolution(solutionID);
+		if (solution == null)
+		{
+			response.status(404);
+			return "{}";
+		}
+
+		final String solutionToken = solution.getToken();
+		final String solutionTokenHeader = request.headers("Solution-Token");
+
+		// NB: we use the assignment resolved via the solution, NOT the one we'd get from assignmentID!
+		// Otherwise, someone could create their own assignment, forge the request with that assignment ID
+		// and a solutionID belonging to a different assignment, and gain access to the solution without having
+		// the token of the assignment it actually belongs to.
+		final String assignmentToken = solution.getAssignment().getToken();
+		final String assignmentTokenHeader = request.headers("Assignment-Token");
+
+		if (!solutionToken.equals(solutionTokenHeader) && !assignmentToken.equals(assignmentTokenHeader))
+		{
+			response.status(401);
+			// language=JSON
+			return "{\n" + "  \"error\": \"invalid token\"\n" + "}\n";
+		}
+
+		final JSONObject obj = toJson(solution);
+		return obj.toString(2);
+	}
+
+	public static Object getAll(Request request, Response response)
+	{
+		final String assignmentID = request.params("assignmentID");
+
+		if (request.contentType() == null || !request.contentType().startsWith("application/json"))
+		{
+			response.redirect("/assignment/solutions.html?id=" + assignmentID);
+			return "";
+		}
+
+		final Assignment assignment = Mongo.get().getAssignment(assignmentID);
+		if (assignment == null)
+		{
+			response.status(404);
+			return unknownAssignmentError(assignmentID);
+		}
+
+		final String token = request.headers("Assignment-Token");
+
+		if (!token.equals(assignment.getToken()))
+		{
+			response.status(401);
+			// language=JSON
+			return "{\n" + "  \"error\": \"invalid token\"\n" + "}\n";
+		}
+
+		final List<Solution> solutions = Mongo.get().getSolutions(assignmentID);
+
+		final JSONObject result = new JSONObject();
+
+		result.put("count", solutions.size());
+
+		final JSONArray array = new JSONArray();
+
+		for (final Solution solution : solutions)
+		{
+			array.put(toJson(solution));
+		}
+
+		result.put("solutions", array);
+
+		return result.toString(2);
+	}
+
+	private static JSONObject toJson(Solution solution)
+	{
+		final JSONObject obj = new JSONObject();
+
+		obj.put(Solution.PROPERTY_id, solution.getID());
+		obj.put(Solution.PROPERTY_assignment, solution.getAssignment().getID());
+		obj.put(Solution.PROPERTY_name, solution.getName());
+		obj.put(Solution.PROPERTY_studentID, solution.getStudentID());
+		obj.put(Solution.PROPERTY_email, solution.getEmail());
+		obj.put(Solution.PROPERTY_solution, solution.getSolution());
+		obj.put(Solution.PROPERTY_timeStamp, solution.getTimeStamp());
+		// don't include token!
+
+		return obj;
+	}
+
+	// --------------- Checking ---------------
 
 	public static Object check(Request request, Response response) throws Exception
 	{
@@ -138,115 +254,7 @@ public class Solutions
 		}
 	}
 
-	private static Solution fromJson(String id, JSONObject obj)
-	{
-		final Solution solution = new Solution(id);
-		solution.setName(obj.getString(Solution.PROPERTY_name));
-		solution.setStudentID(obj.getString(Solution.PROPERTY_studentID));
-		solution.setEmail(obj.getString(Solution.PROPERTY_email));
-		solution.setSolution(obj.getString(Solution.PROPERTY_solution));
-		// don't include token!
-		return solution;
-	}
-
-	public static Object get(Request request, Response response)
-	{
-		final String assignmentID = request.params("assignmentID");
-		final String solutionID = request.params("solutionID");
-
-		if (request.contentType() == null || !request.contentType().startsWith("application/json"))
-		{
-			response.redirect("/assignment/solution.html?id=" + assignmentID + "&solution=" + solutionID);
-			return "";
-		}
-
-		final Solution solution = Mongo.get().getSolution(solutionID);
-		if (solution == null)
-		{
-			response.status(404);
-			return "{}";
-		}
-
-		final String solutionToken = solution.getToken();
-		final String solutionTokenHeader = request.headers("Solution-Token");
-
-		// NB: we use the assignment resolved via the solution, NOT the one we'd get from assignmentID!
-		// Otherwise, someone could create their own assignment, forge the request with that assignment ID
-		// and a solutionID belonging to a different assignment, and gain access to the solution without having
-		// the token of the assignment it actually belongs to.
-		final String assignmentToken = solution.getAssignment().getToken();
-		final String assignmentTokenHeader = request.headers("Assignment-Token");
-
-		if (!solutionToken.equals(solutionTokenHeader) && !assignmentToken.equals(assignmentTokenHeader))
-		{
-			response.status(401);
-			// language=JSON
-			return "{\n" + "  \"error\": \"invalid token\"\n" + "}\n";
-		}
-
-		final JSONObject obj = toJson(solution);
-		return obj.toString(2);
-	}
-
-	private static JSONObject toJson(Solution solution)
-	{
-		final JSONObject obj = new JSONObject();
-
-		obj.put(Solution.PROPERTY_id, solution.getID());
-		obj.put(Solution.PROPERTY_assignment, solution.getAssignment().getID());
-		obj.put(Solution.PROPERTY_name, solution.getName());
-		obj.put(Solution.PROPERTY_studentID, solution.getStudentID());
-		obj.put(Solution.PROPERTY_email, solution.getEmail());
-		obj.put(Solution.PROPERTY_solution, solution.getSolution());
-		obj.put(Solution.PROPERTY_timeStamp, solution.getTimeStamp());
-		// don't include token!
-
-		return obj;
-	}
-
-	public static Object getAll(Request request, Response response)
-	{
-		final String assignmentID = request.params("assignmentID");
-
-		if (request.contentType() == null || !request.contentType().startsWith("application/json"))
-		{
-			response.redirect("/assignment/solutions.html?id=" + assignmentID);
-			return "";
-		}
-
-		final Assignment assignment = Mongo.get().getAssignment(assignmentID);
-		if (assignment == null)
-		{
-			response.status(404);
-			return unknownAssignmentError(assignmentID);
-		}
-
-		final String token = request.headers("Assignment-Token");
-
-		if (!token.equals(assignment.getToken()))
-		{
-			response.status(401);
-			// language=JSON
-			return "{\n" + "  \"error\": \"invalid token\"\n" + "}\n";
-		}
-
-		final List<Solution> solutions = Mongo.get().getSolutions(assignmentID);
-
-		final JSONObject result = new JSONObject();
-
-		result.put("count", solutions.size());
-
-		final JSONArray array = new JSONArray();
-
-		for (final Solution solution : solutions)
-		{
-			array.put(toJson(solution));
-		}
-
-		result.put("solutions", array);
-
-		return result.toString(2);
-	}
+	// --------------- Helpers ---------------
 
 	private static String unknownAssignmentError(String assignmentID)
 	{
