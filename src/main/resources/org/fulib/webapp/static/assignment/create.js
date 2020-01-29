3869f0029da89d9f321b64217ac0bf94e8edbd29
Adrian Kunz <clashsoft@hotmail.com>
2020-01-29 20:41:06 +0100

    Restructure create.js

--- src/main/resources/org/fulib/webapp/static/assignment/create.js
+++ src/main/resources/org/fulib/webapp/static/assignment/create.js
@@ -41,8 +41,13 @@ const verificationConfig = {
 // =============== Initialization ===============
 
 (() => {
-	try {
-		// may fail if darktheme/network is unavailable
+	try { // may fail if darktheme/network is unavailable
+		function updateEditorTheme(theme = getTheme()) {
+			let editorTheme = theme === 'dark' ? 'darcula' : 'idea';
+			solutionInputCM.setOption('theme', editorTheme);
+			verificationInputCM.setOption('theme', editorTheme);
+		}
+
 		updateEditorTheme();
 		themeChangeHandlers.push(updateEditorTheme);
 	} catch {}
@@ -63,11 +68,71 @@ const verificationConfig = {
 
 // =============== Functions ===============
 
-function updateEditorTheme(theme = getTheme()) {
-	let editorTheme = theme === 'dark' ? 'darcula' : 'idea';
-	solutionInputCM.setOption('theme', editorTheme);
-	verificationInputCM.setOption('theme', editorTheme);
+// --------------- Events ---------------
+
+function onExport() {
+	downloadJson(gatherData(), titleInput.value);
 }
+
+function onImport() {
+	const file = importFile.files[0];
+	readJson(file, data => {
+		this.loadData(data);
+	});
+}
+
+function onRemoveTask(id) {
+	if (confirm('Are you sure you want to remove this task?')) {
+		removeTask(id);
+	}
+}
+
+function onSubmit() {
+	submitButton.disabled = true;
+	submitButton.innerText = 'Submitting...';
+
+	const data = gatherData();
+
+	api('POST', '/assignment', data, result => {
+		const link = absoluteLink(`/assignment/${result.id}`);
+		const solutionsLinkRef = link + '/solutions';
+
+		submitButton.disabled = false;
+		submitButton.innerText = 'Submit';
+
+		titleLabel.innerText = titleInput.value;
+		assignmentLink.href = link;
+		assignmentLink.innerText = link;
+		solutionsLink.href = solutionsLinkRef;
+		solutionsLink.innerText = solutionsLinkRef;
+		tokenLabel.innerText = result.token;
+		localStorage.setItem(`assignment/${result.id}/token`, result.token);
+
+		$('#successModal').modal('show');
+	});
+}
+
+function onCopyAssignmentLink() {
+	copyToClipboard(assignmentLink.innerText);
+	animateCopyButton(copyAssignmentLinkButton);
+}
+
+function onCopySolutionsLink() {
+	copyToClipboard(solutionsLink.innerText);
+	animateCopyButton(copySolutionsLinkButton);
+}
+
+function onCopyToken() {
+	copyToClipboard(tokenLabel.innerText);
+	animateCopyButton(copyTokenButton);
+}
+
+function animateCopyButton(button) {
+	button.innerText = 'Copied!';
+	setTimeout(() => button.innerText = 'Copy', 5000);
+}
+
+// --------------- Data ---------------
 
 function gatherData() {
 	const data = {
@@ -114,63 +179,15 @@ function loadData(data) {
 	saveTaskIDs(taskIDs);
 }
 
-function onExport() {
-	downloadJson(gatherData(), titleInput.value);
-}
-
-function onImport() {
-	const file = importFile.files[0];
-	readJson(file, data => {
-		this.loadData(data);
-	});
-}
-
-function submit() {
-	submitButton.disabled = true;
-	submitButton.innerText = 'Submitting...';
-
-	const data = gatherData();
-
-	api('POST', '/assignment', data, result => {
-		const link = absoluteLink(`/assignment/${result.id}`);
-		const solutionsLinkRef = link + '/solutions';
-
-		submitButton.disabled = false;
-		submitButton.innerText = 'Submit';
-
-		titleLabel.innerText = titleInput.value;
-		assignmentLink.href = link;
-		assignmentLink.innerText = link;
-		solutionsLink.href = solutionsLinkRef;
-		solutionsLink.innerText = solutionsLinkRef;
-		tokenLabel.innerText = result.token;
-		localStorage.setItem(`assignment/${result.id}/token`, result.token);
-
-		$('#successModal').modal('show');
-	});
-}
-
-function animateCopyButton(button) {
-	button.innerText = 'Copied!';
-	setTimeout(() => button.innerText = 'Copy', 5000);
-}
-
-function onCopyAssignmentLink() {
-	copyToClipboard(assignmentLink.innerText);
-	animateCopyButton(copyAssignmentLinkButton);
-}
-
-function onCopySolutionsLink() {
-	copyToClipboard(solutionsLink.innerText);
-	animateCopyButton(copySolutionsLinkButton);
-}
-
-function onCopyToken() {
-	copyToClipboard(tokenLabel.innerText);
-	animateCopyButton(copyTokenButton);
-}
-
 // --------------- Tasks ---------------
+
+function loadTasks() {
+	const taskIDs = loadTaskIDs();
+	removeChildren(taskList);
+	for (const id of taskIDs) {
+		addTask(id);
+	}
+}
 
 function loadTaskIDs() {
 	const taskIDString = localStorage.getItem('assignment/create/taskIDs');
@@ -265,18 +282,4 @@ function clearTasks() {
 	}
 
 	saveTaskIDs([]);
-}
-
-function onRemoveTask(id) {
-	if (confirm('Are you sure you want to remove this task?')) {
-		removeTask(id);
-	}
-}
-
-function loadTasks() {
-	const taskIDs = loadTaskIDs();
-	removeChildren(taskList);
-	for (const id of taskIDs) {
-		addTask(id);
-	}
 }
