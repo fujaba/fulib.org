54a1fc7c5bca931f62923af93f91bd600744bdce
Adrian Kunz <clashsoft@hotmail.com>
2020-02-02 14:09:21 +0100

    Use new APIs merged from assignments

--- src/main/java/org/fulib/webapp/assignment/Solutions.java
+++ src/main/java/org/fulib/webapp/assignment/Solutions.java
@@ -194,19 +194,8 @@ public class Solutions
 	public static Object getAssignee(Request request, Response response)
 	{
 		final String solutionID = request.params("solutionID");
-		final Solution solution = Mongo.get().getSolution(solutionID);
-
-		if (solution == null)
-		{
-			response.status(404);
-			return String.format(UNKNOWN_SOLUTION_RESPONSE, solutionID);
-		}
-
-		if (!Assignments.isAuthorized(request, solution.getAssignment()))
-		{
-			response.status(401);
-			return INVALID_TOKEN_RESPONSE;
-		}
+		final Solution solution = getSolutionOr404(solutionID);
+		Assignments.checkPrivilege(request, solution.getAssignment());
 
 		final JSONObject result = new JSONObject();
 		result.put(Solution.PROPERTY_assignee, solution.getAssignee());
@@ -216,23 +205,11 @@ public class Solutions
 	public static Object setAssignee(Request request, Response response)
 	{
 		final String solutionID = request.params("solutionID");
+		final Solution solution = getSolutionOr404(solutionID);
+		Assignments.checkPrivilege(request, solution.getAssignment());
 
 		final JSONObject body = new JSONObject(request.body());
 		final String assignee = body.getString(Solution.PROPERTY_assignee);
-
-		final Solution solution = Mongo.get().getSolution(solutionID);
-
-		if (solution == null)
-		{
-			response.status(404);
-			return String.format(UNKNOWN_SOLUTION_RESPONSE, solutionID);
-		}
-
-		if (!Assignments.isAuthorized(request, solution.getAssignment()))
-		{
-			response.status(401);
-			return INVALID_TOKEN_RESPONSE;
-		}
 
 		Mongo.get().saveAssignee(solutionID, assignee);
 
