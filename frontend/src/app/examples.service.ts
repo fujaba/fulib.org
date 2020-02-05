bfe92a695e88a0d1ba1955beeb1c8be6fa84db40
Adrian Kunz <clashsoft@hotmail.com>
2020-02-06 00:47:38 +0100

    Add categories and examples to examples service

--- frontend/src/app/examples.service.ts
+++ frontend/src/app/examples.service.ts
@@ -1,9 +1,55 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import ExampleCategory from "./model/example-category";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExamplesService {
+  private readonly categories: ExampleCategory[] = [
+    {
+      name: 'Definitions', examples: [
+        {name: 'Basics'},
+        {name: 'Simple Definitions'},
+        {name: 'Complex Definitions'},
+        {name: 'Associations'},
+        {name: 'Special Associations'},
+      ]
+    },
+    {
+      name: 'Testing', examples: [
+        {name: 'Expectations'},
+        {name: 'Relational Operators'},
+        {name: 'Object Diagrams'},
+      ]
+    },
+    {
+      name: 'Transformation', examples: [
+        {name: 'Lists'},
+        {name: 'Modifying Data'},
+        {name: 'Conditionals'},
+        {name: 'Loops'},
+      ]
+    },
+    {
+      name: 'Methods', examples: [
+        {name: 'Calling Methods'},
+        {name: 'Passing Arguments'},
+        {name: 'Piecewise Definition'},
+      ]
+    },
+    {
+      name: 'Prototyping', examples: [
+        {name: 'Data Model'},
+        {name: 'Web Gui'},
+        {name: 'Prototype'},
+      ]
+    },
+  ];
 
-  constructor() { }
+  constructor() {
+  }
+
+  getCategories(): ExampleCategory[] {
+    return this.categories;
+  }
 }
