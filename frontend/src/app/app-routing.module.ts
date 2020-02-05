3773d49528f6e160b9a5f1d517ce8d4a4b05bbd2
Adrian Kunz <clashsoft@hotmail.com>
2020-02-05 23:51:41 +0100

    Add route for four-pane editor

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FourPaneEditorComponent} from "./component/four-pane-editor/four-pane-editor.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FourPaneEditorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
