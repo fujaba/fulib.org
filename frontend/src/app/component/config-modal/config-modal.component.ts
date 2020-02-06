2be075695d52cd16b6445616f2cec19272521e88
Adrian Kunz <clashsoft@hotmail.com>
2020-02-06 12:53:34 +0100

    Load from and save to scenario editor service in config modal component

--- frontend/src/app/component/config-modal/config-modal.component.ts
+++ frontend/src/app/component/config-modal/config-modal.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ScenarioEditorService} from "../../scenario-editor.service";
 
 declare var $: any;
 
@@ -10,21 +11,30 @@ declare var $: any;
 export class ConfigModalComponent implements OnInit {
   packageName: string;
   projectName: any;
-  version: any;
+  projectVersion: any;
   scenarioFileName: any;
 
-  constructor() {
+  constructor(
+    private scenarioEditorService: ScenarioEditorService,
+  ) {
   }
 
   ngOnInit() {
   }
 
   open(): void {
+    this.packageName = this.scenarioEditorService.packageName;
+    this.projectName = this.scenarioEditorService.projectName;
+    this.projectVersion = this.scenarioEditorService.projectVersion;
+    this.scenarioFileName = this.scenarioEditorService.scenarioFileName;
     $('#configModal').modal('show');
   }
 
   private save(): void {
-
+    this.scenarioEditorService.packageName = this.packageName;
+    this.scenarioEditorService.projectName = this.projectName;
+    this.scenarioEditorService.projectVersion = this.projectVersion;
+    this.scenarioEditorService.scenarioFileName = this.scenarioFileName;
   }
 
   private downloadProjectZip(): void {
