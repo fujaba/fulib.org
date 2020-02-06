64e7941090dce9f84c81864e338809f3407629fa
Adrian Kunz <clashsoft@hotmail.com>
2020-02-06 13:38:22 +0100

    Add safeHtml pipe

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { HttpClientModule } from '@angular/common/http'
 import { CodemirrorModule } from '@ctrl/ngx-codemirror';
 
 import { AppRoutingModule } from './app-routing.module';
+import {SafeHtmlPipe} from "./pipes/safe-html.pipe";
 import { AppComponent } from './component/app.component';
 import { FourPaneEditorComponent } from './component/four-pane-editor/four-pane-editor.component';
 import { ConfigModalComponent } from './component/config-modal/config-modal.component';
 
 @NgModule({
   declarations: [
+    SafeHtmlPipe,
     AppComponent,
     FourPaneEditorComponent,
     ConfigModalComponent
