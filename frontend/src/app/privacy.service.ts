4852066bdeec8278a904d7c643a3304f009699c5
Adrian Kunz <clashsoft@hotmail.com>
2020-02-06 17:22:29 +0100

    Add privacy property to privacy service

--- frontend/src/app/privacy.service.ts
+++ frontend/src/app/privacy.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
 
+type Privacy = 'all' | 'local' | 'none' | 'nobanner';
+
 @Injectable({
   providedIn: 'root'
 })
 export class PrivacyService {
+  private _privacy: Privacy | null;
 
   constructor() { }
+
+  get privacy(): Privacy | null {
+    return this._privacy || localStorage.getItem('privacy') as Privacy;
+  }
+
+  set privacy(value: Privacy | null) {
+    if (this._privacy !== value) {
+      this._privacy = value;
+      localStorage.setItem('privacy', value);
+    }
+  }
 }
