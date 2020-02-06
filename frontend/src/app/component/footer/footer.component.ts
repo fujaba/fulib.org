49197269e318efe34aa5ed5e9c2966af762b58b1
Adrian Kunz <clashsoft@hotmail.com>
2020-02-06 17:14:09 +0100

    Implement modals in footer with ng-bootstrap

--- frontend/src/app/component/footer/footer.component.ts
+++ frontend/src/app/component/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    public readonly modalService: NgbModal,
+  ) {
+  }
 
   ngOnInit() {
   }
 
+  savePrivacy() {
+
+  }
 }
