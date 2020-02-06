e74ad3bd670b8e669d33ea2dded4b9b31afd1255
Adrian Kunz <clashsoft@hotmail.com>
2020-02-06 01:41:24 +0100

    Add stored scenario with persistence to scenario editor service

--- frontend/src/app/scenario-editor.service.ts
+++ frontend/src/app/scenario-editor.service.ts
@@ -4,6 +4,24 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ScenarioEditorService {
+  private persistenceKey = 'storedScenario';
+  private defaultScenario = `# My First Scenario
+
+// start typing your scenario or select an example using the dropdown above.
+
+There is a Car with name Herbie.
+`;
+
+  private _storedScenario?: string;
 
   constructor() { }
+
+  get storedScenario(): string {
+    return this._storedScenario || localStorage.getItem(this.persistenceKey) || this.defaultScenario;
+  }
+
+  set storedScenario(value: string) {
+    this._storedScenario = value;
+    localStorage.setItem(this.persistenceKey, value);
+  }
 }
