5eb71a2793760e46cb9cde9ec382dbad6274060b
Adrian Kunz <clashsoft@hotmail.com>
2020-02-06 17:58:00 +0100

    Implement dark switch component

--- frontend/src/app/dark-switch/dark-switch.component.ts
+++ frontend/src/app/dark-switch/dark-switch.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+declare function setTheme(theme: string);
+declare function getTheme(): string;
+declare const themeChangeHandlers: (() => void)[];
+
 @Component({
   selector: 'app-dark-switch',
   templateUrl: './dark-switch.component.html',
   styleUrls: ['./dark-switch.component.scss']
 })
 export class DarkSwitchComponent implements OnInit {
+  private _enabled: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.loadEnabled();
   }
 
+  private loadEnabled(): void {
+    this._enabled = getTheme() === 'dark';
+  }
+
+  get enabled(): boolean {
+    return this._enabled;
+  }
+
+  set enabled(value: boolean) {
+    this._enabled = value;
+    setTheme(value ? 'dark' : 'light');
+  }
 }
