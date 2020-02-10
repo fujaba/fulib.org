9da31a2db2e02f0e40cf822a6f3e424793d3e92a
Adrian Kunz <clashsoft@hotmail.com>
2020-02-10 20:29:21 +0100

    Connect inputs in assignment create to controller

--- src/main/angular/src/app/assignment/create/create.component.ts
+++ src/main/angular/src/app/assignment/create/create.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-create',
@@ -8,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class CreateComponent implements OnInit {
   importFile: File;
 
-  constructor() { }
+  title: string;
+  author: string;
+  email: string;
+  deadlineDate: string;
+  deadlineTime: string;
+  description: string = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+laborum.`.replace(/\s+/g, ' ');
+  sampleSolution: string;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
