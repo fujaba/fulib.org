8500227c71e7f7ae778afec759fa287a90c86bf0
Adrian Kunz <clashsoft@hotmail.com>
2020-02-10 20:15:34 +0100

    Setup assignment create route

--- src/main/angular/src/app/app-routing.module.ts
+++ src/main/angular/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {FourPaneEditorComponent} from "./four-pane-editor/four-pane-editor.component";
+import {CreateComponent} from './assignment/create/create.component';
 
 const routes: Routes = [
   {path: '', component: FourPaneEditorComponent},
+  {path: 'assignment/create', component: CreateComponent}
 ];
 
 @NgModule({
