9fa6cf3da132767b34a993730d6a00fbb71abc73
Adrian Kunz <clashsoft@hotmail.com>
2020-02-10 21:00:32 +0100

    Implement autotheme-codemirror forwarding to ngx-codemirror

--- src/main/angular/src/app/autotheme-codemirror/autotheme-codemirror.component.ts
+++ src/main/angular/src/app/autotheme-codemirror/autotheme-codemirror.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-autotheme-codemirror',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./autotheme-codemirror.component.scss']
 })
 export class AutothemeCodemirrorComponent implements OnInit {
+  @Input() content: string;
+  @Output() contentChange = new EventEmitter<string>();
+
+  @Input() options: any;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  setContent(value: string) {
+    this.content = value;
+    this.contentChange.emit(value);
+  }
 }
