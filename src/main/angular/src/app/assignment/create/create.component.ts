bf603ff6ab0ab116f0bff9a3e8c6fdb5b6493aa4
Adrian Kunz <clashsoft@hotmail.com>
2020-02-10 22:04:15 +0100

    Remove default description

--- src/main/angular/src/app/assignment/create/create.component.ts
+++ src/main/angular/src/app/assignment/create/create.component.ts
@@ -16,11 +16,7 @@ export class CreateComponent implements OnInit {
   email: string;
   deadlineDate: Date;
   deadlineTime: Date;
-  description: string = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-laborum.`.replace(/\s+/g, ' ');
+  description: string;
   solution: string;
 
   tasks: Task[] = [];
