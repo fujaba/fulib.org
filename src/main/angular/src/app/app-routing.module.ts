2431372ba7520602c6106924852c429d5ceebd79
Adrian Kunz <clashsoft@hotmail.com>
2020-02-11 22:27:32 +0100

    Setup routing for solve component

--- src/main/angular/src/app/app-routing.module.ts
+++ src/main/angular/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import {RouterModule, Routes} from '@angular/router';
 
 import {FourPaneEditorComponent} from "./four-pane-editor/four-pane-editor.component";
 import {CreateComponent} from './assignment/create/create.component';
+import {SolveComponent} from './assignment/solve/solve.component';
 
 const routes: Routes = [
   {path: '', component: FourPaneEditorComponent},
-  {path: 'assignment/create', component: CreateComponent}
+  {path: 'assignment/create', component: CreateComponent},
+  {path: 'assignment/:id', component: SolveComponent},
 ];
 
 @NgModule({
