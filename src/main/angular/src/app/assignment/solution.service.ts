9ad8872211ddfe741fbedbeabd537f3bb4de5ed9
Adrian Kunz <clashsoft@hotmail.com>
2020-02-11 23:20:55 +0100

    Add submit to solution service

--- src/main/angular/src/app/assignment/solution.service.ts
+++ src/main/angular/src/app/assignment/solution.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+import Solution from './model/solution';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SolutionService {
+  constructor(
+    private http: HttpClient,
+  ) {
+  }
 
-  constructor() { }
+  submit(solution: Solution): Observable<Solution> {
+    return this.http.post<Solution>(`${environment.apiURL}/assignments/${solution.assignment.id}/solutions`, solution).pipe(
+      map(partialResult => {
+        const result = new Solution();
+        Object.assign(result, solution);
+        Object.assign(result, partialResult);
+        return result;
+      })
+    );
+  }
 }
