72f8568a5da787e4cadd93d51d10b5cd323da1b5
Adrian Kunz <clashsoft@hotmail.com>
2020-02-12 19:34:20 +0100

    Setup route for solution component

--- src/main/angular/src/app/app-routing.module.ts
+++ src/main/angular/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {FourPaneEditorComponent} from "./four-pane-editor/four-pane-editor.component";
 import {CreateComponent} from './assignment/create/create.component';
 import {SolveComponent} from './assignment/solve/solve.component';
+import {SolutionComponent} from './assignment/solution/solution.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: '', component: FourPaneEditorComponent},
   {path: 'assignments/create', component: CreateComponent},
   {path: 'assignments/:id', component: SolveComponent},
+  {path: 'assignments/:aid/solutions/:sid', component: SolutionComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
 
