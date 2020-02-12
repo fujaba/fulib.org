7412e0440ac2f3b3ee8f628e78d3622530e2b207
Adrian Kunz <clashsoft@hotmail.com>
2020-02-12 20:25:32 +0100

    Implement solution component

--- src/main/angular/src/app/assignment/solution/solution.component.ts
+++ src/main/angular/src/app/assignment/solution/solution.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+import {SolutionService} from '../solution.service';
+import {AssignmentService} from '../assignment.service';
+
+import Assignment from '../model/assignment';
+import Solution from '../model/solution';
 
 @Component({
   selector: 'app-solution',
@@ -6,10 +13,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./solution.component.scss']
 })
 export class SolutionComponent implements OnInit {
+  assignment?: Assignment;
+  solution?: Solution;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+    private assignmentService: AssignmentService,
+    private solutionService: SolutionService,
+  ) {
   }
 
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      const assignmentID = params.aid;
+      const solutionID = params.sid;
+      this.assignmentService.get(assignmentID).subscribe(assignment => {
+        this.assignment = assignment;
+      });
+      this.solutionService.get(assignmentID, solutionID).subscribe(solution => {
+        this.solution = solution;
+      })
+    })
+  }
 }
