79c9f6701c92e8dc1cf13ad25f339690d74fe571
Adrian Kunz <clashsoft@hotmail.com>
2020-02-12 22:55:00 +0100

    Show solution table in solution list component

--- src/main/angular/src/app/assignment/solution-list/solution-list.component.ts
+++ src/main/angular/src/app/assignment/solution-list/solution-list.component.ts
@@ -3,6 +3,8 @@ import {ActivatedRoute} from '@angular/router';
 
 import Assignment from '../model/assignment';
 import {AssignmentService} from '../assignment.service';
+import Solution from '../model/solution';
+import {SolutionService} from '../solution.service';
 
 @Component({
   selector: 'app-solution-list',
@@ -12,10 +14,13 @@ import {AssignmentService} from '../assignment.service';
 export class SolutionListComponent implements OnInit {
   assignmentID: string;
   assignment?: Assignment;
+  totalPoints?: number;
+  solutions?: Solution[];
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private assignmentService: AssignmentService,
+    private solutionService: SolutionService,
   ) {
   }
 
@@ -24,7 +29,19 @@ export class SolutionListComponent implements OnInit {
       this.assignmentID = params.id;
       this.assignmentService.get(this.assignmentID).subscribe(assignment => {
         this.assignment = assignment;
+        this.totalPoints = this.sumPoints(assignment.tasks);
+      });
+      this.solutionService.getAll(this.assignmentID).subscribe(solutions => {
+        this.solutions = solutions;
       });
     });
+  }
+
+  totalResultPoints(solution: Solution) {
+    return this.sumPoints(solution.results);
+  }
+
+  sumPoints(arr: {points: number}[]) {
+    return arr.reduce((acc, item) => acc + item.points, 0);
   }
 }
