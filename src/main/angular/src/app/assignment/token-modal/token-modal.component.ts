8e88dea51ba5f8263462600b26892efbe9d0b1cd
Adrian Kunz <clashsoft@hotmail.com>
2020-02-12 21:44:17 +0100

    Implement token modal component

--- src/main/angular/src/app/assignment/token-modal/token-modal.component.ts
+++ src/main/angular/src/app/assignment/token-modal/token-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-token-modal',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./token-modal.component.scss']
 })
 export class TokenModalComponent implements OnInit {
+  @ViewChild('tokenModal', {static: true}) tokenModal;
 
-  constructor() { }
+  @Output() submit = new EventEmitter<{ solutionToken: string; assignmentToken: string; }>();
+
+  solutionToken: string;
+  assignmentToken: string;
+
+  constructor(
+    private modalService: NgbModal,
+  ) {
+  }
 
   ngOnInit() {
   }
 
+  open(): void {
+    this.modalService.open(this.tokenModal, {ariaLabelledBy: 'tokenModalLabel'});
+  }
+
+  submitTokens(): void {
+    this.submit.emit({
+      solutionToken: this.solutionToken,
+      assignmentToken: this.assignmentToken,
+    });
+  }
 }
