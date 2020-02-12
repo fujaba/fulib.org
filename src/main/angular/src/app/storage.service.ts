44cfc875567e2b1234755cde3013ded10644cc0d
Adrian Kunz <clashsoft@hotmail.com>
2020-02-12 21:16:20 +0100

    Add get and set methods to storage service

--- src/main/angular/src/app/storage.service.ts
+++ src/main/angular/src/app/storage.service.ts
@@ -4,6 +4,27 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class StorageService {
+  private properties: Map<string, string>;
 
-  constructor() { }
+  constructor() {
+  }
+
+  get(key: string): string | null {
+    let token = this.properties.get(key);
+    if (typeof token == 'undefined') {
+      token = localStorage.getItem(key);
+      this.properties.set(key, token);
+    }
+    return token;
+  }
+
+  set(key: string, value: string | null) {
+    this.properties.set(key, value);
+    if (value) {
+      localStorage.setItem(key, value);
+    }
+    else {
+      localStorage.removeItem(key);
+    }
+  }
 }
