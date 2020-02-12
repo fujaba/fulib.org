c285c1b2d8406fbde04f3199706e06281b9f2927
Adrian Kunz <clashsoft@hotmail.com>
2020-02-12 22:16:07 +0100

    Add route for solution list

--- src/main/angular/src/app/app-routing.module.ts
+++ src/main/angular/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {FourPaneEditorComponent} from "./four-pane-editor/four-pane-editor.component";
 import {CreateComponent} from './assignment/create/create.component';
 import {SolveComponent} from './assignment/solve/solve.component';
+import {SolutionListComponent} from './assignment/solution-list/solution-list.component';
 import {SolutionComponent} from './assignment/solution/solution.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path: '', component: FourPaneEditorComponent},
   {path: 'assignments/create', component: CreateComponent},
   {path: 'assignments/:id', component: SolveComponent},
+  {path: 'assignments/:id/solutions', component: SolutionListComponent},
   {path: 'assignments/:aid/solutions/:sid', component: SolutionComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
