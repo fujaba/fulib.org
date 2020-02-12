869f0c7a60e569931fc09821c54c4074d511f324
Adrian Kunz <clashsoft@hotmail.com>
2020-02-12 21:24:48 +0100

    Initialize properties in storage service

--- src/main/angular/src/app/storage.service.ts
+++ src/main/angular/src/app/storage.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class StorageService {
-  private properties: Map<string, string>;
+  private properties = new Map<string, string>();
 
   constructor() {
   }
