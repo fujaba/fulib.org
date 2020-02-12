b6802b8859a1f3cc649608ae155079d63cb95125
Adrian Kunz <clashsoft@hotmail.com>
2020-02-12 19:30:55 +0100

    Add route for 404

--- src/main/angular/src/app/app-routing.module.ts
+++ src/main/angular/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {FourPaneEditorComponent} from "./four-pane-editor/four-pane-editor.component";
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: '', component: FourPaneEditorComponent},
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
