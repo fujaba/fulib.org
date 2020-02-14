27b5854bf13e4f13f7d03d1092a52f467c90b219
Adrian Kunz <clashsoft@hotmail.com>
2020-02-14 11:15:59 +0100

    Generate assignment module

---
+++ src/main/angular/src/app/assignment/assignment.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {AssignmentRoutingModule} from './assignment-routing.module';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    AssignmentRoutingModule
+  ]
+})
+export class AssignmentModule {
+}
