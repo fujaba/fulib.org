64d3980a857c81a702cef3cdf65fa49ef0c4fa4e
Adrian Kunz <clashsoft@hotmail.com>
2020-02-14 11:52:11 +0100

    Make SafeHtmlPipe, DarkSwitch and AutothemeCodemirror part of Shared Module

--- src/main/angular/src/app/app.module.ts
+++ src/main/angular/src/app/app.module.ts
@@ -3,31 +3,25 @@ import {FormsModule} from '@angular/forms';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http'
 
-import {CodemirrorModule} from '@ctrl/ngx-codemirror';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-import {AppRoutingModule} from './app-routing.module';
+import {SharedModule} from './shared/shared.module';
 
-import {SafeHtmlPipe} from "./pipes/safe-html.pipe";
+import {AppRoutingModule} from './app-routing.module';
 
 import {AppComponent} from './app.component';
 import {FourPaneEditorComponent} from './four-pane-editor/four-pane-editor.component';
 import {ConfigModalComponent} from './config-modal/config-modal.component';
 import {FooterComponent} from './footer/footer.component';
 
-import {DarkSwitchComponent} from './dark-switch/dark-switch.component';
-import { AutothemeCodemirrorComponent } from './autotheme-codemirror/autotheme-codemirror.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    SafeHtmlPipe,
     AppComponent,
     FourPaneEditorComponent,
     ConfigModalComponent,
     FooterComponent,
-    DarkSwitchComponent,
-    AutothemeCodemirrorComponent,
     PageNotFoundComponent,
   ],
   imports: [
@@ -35,7 +29,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     FormsModule,
     HttpClientModule,
     NgbModule,
-    CodemirrorModule,
+    SharedModule,
     AppRoutingModule,
   ],
   providers: [],
