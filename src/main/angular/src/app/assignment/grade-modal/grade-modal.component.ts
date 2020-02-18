d095f2a7bd8c4430493e19f59e3c04b4a65d532a
Adrian Kunz <clashsoft@hotmail.com>
2020-02-18 14:14:34 +0100

    Add grade modal template

--- src/main/angular/src/app/assignment/grade-modal/grade-modal.component.ts
+++ src/main/angular/src/app/assignment/grade-modal/grade-modal.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-grade-modal',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grade-modal.component.scss']
 })
 export class GradeModalComponent implements OnInit {
+  @ViewChild('gradeModal', {static: true}) gradeModal;
 
-  constructor() { }
+  name: string;
+  points: number;
+  note: string;
+
+  constructor(
+    private modalService: NgbModal,
+  ) {
+  }
 
   ngOnInit() {
   }
 
+  open(): void {
+    this.modalService.open(this.gradeModal, {ariaLabelledBy: 'gradeModalLabel'});
+  }
+
+  submit() {
+    // TODO
+  }
 }
