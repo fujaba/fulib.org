95c510f28ccb81ab216856e91f4a7655b8b61eba
Adrian Kunz <clashsoft@hotmail.com>
2020-02-18 14:08:14 +0100

    Generate grade modal

---
+++ src/main/angular/src/app/assignment/grade-modal/grade-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grade-modal',
+  templateUrl: './grade-modal.component.html',
+  styleUrls: ['./grade-modal.component.scss']
+})
+export class GradeModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
