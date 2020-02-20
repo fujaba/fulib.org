2f63a4054578948bf91ddd00ded4d4d4bd1e084f
Adrian Kunz <clashsoft@hotmail.com>
2020-02-20 14:08:41 +0100

    Implement changelog component

--- src/main/angular/src/app/changelog/changelog.component.ts
+++ src/main/angular/src/app/changelog/changelog.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+
+import {ChangelogService} from '../changelog.service';
 
 @Component({
   selector: 'app-changelog',
   templateUrl: './changelog.component.html',
   styleUrls: ['./changelog.component.scss']
 })
-export class ChangelogComponent implements OnInit {
+export class ChangelogComponent implements OnInit, AfterViewInit {
+  @ViewChild('changelogModal', {static: true}) changelogModal;
 
-  constructor() { }
+  changelog: string;
+
+  constructor(
+    private modalService: NgbModal,
+    private changelogService: ChangelogService,
+  ) { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.loadChangelog();
+    this.updateLastUsedVersion();
+  }
+
+  private loadChangelog() {
+    const lastUsedVersions = this.changelogService.lastUsedVersions;
+    if (!lastUsedVersions) {
+      // never used the website before, they probably don't care about the changelogs
+      return;
+    }
+
+    const lastUsedVersion = lastUsedVersions['fulib.org'];
+    this.changelogService.getChangelog('fulib.org', lastUsedVersion).subscribe(changelog => {
+      this.changelog = changelog;
+      this.modalService.open(this.changelogModal, {ariaLabelledBy: 'changelogModalLabel', size: 'xl'});
+    });
+  }
+
+  private updateLastUsedVersion() {
+    this.changelogService.lastUsedVersions = this.changelogService.currentVersions;
+  }
 }
