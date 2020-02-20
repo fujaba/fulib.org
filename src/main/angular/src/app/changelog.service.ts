ceeec3d38f36fc3f2f86af9e949a401b888acb05
Adrian Kunz <clashsoft@hotmail.com>
2020-02-20 13:26:40 +0100

    Add Versions model class and accessors for current and last used versions to changelog service

--- src/main/angular/src/app/changelog.service.ts
+++ src/main/angular/src/app/changelog.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {PrivacyService} from './privacy.service';
+
+export class Versions {
+  webapp: string;
+  fulib: string;
+  fulibTools: string;
+  fulibScenarios: string;
+  fulibMockups: string;
+  fulibGradle: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChangelogService {
+  constructor(
+    private privacyService: PrivacyService,
+  ) {
+  }
 
-  constructor() { }
+  public get currentVersion(): Versions {
+    return {
+      webapp: '$$version$$',
+      fulib: '$$fulibVersion$$',
+      fulibTools: '$$fulibToolsVersion$$',
+      fulibScenarios: '$$fulibScenariosVersion$$',
+      fulibMockups: '$$fulibMockupsVersion$$',
+      fulibGradle: '$$fulibGradleVersion$$',
+    };
+  };
+
+  public get lastUsedVersions(): Versions | null {
+    const stored = this.privacyService.getStorage('lastUsedVersions');
+    return stored ? {...JSON.parse(stored)} as Versions : null;
+  }
+
+  public set lastUsedVersions(value: Versions | null) {
+    if (value) {
+      this.privacyService.setStorage('lastUsedVersions', JSON.stringify(value));
+    } else {
+      localStorage.removeItem('lastUsedVersions');
+    }
+  }
 }
