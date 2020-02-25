4a34517a9f080b45c5aba2eb1ad817dbd3e6d9f0
Adrian Kunz <clashsoft@hotmail.com>
2020-02-25 14:48:33 +0100

    Show points from last grading in task list

--- src/main/angular/src/app/assignment/task-list/task-list.component.ts
+++ src/main/angular/src/app/assignment/task-list/task-list.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import Assignment from '../model/assignment';
 import TaskResult from '../model/task-result';
 import Solution from '../model/solution';
+import TaskGrading from '../model/task-grading';
 
 @Component({
   selector: 'app-task-list',
@@ -12,6 +13,7 @@ export class TaskListComponent implements OnInit {
   @Input() assignment?: Assignment;
   @Input() solution?: Solution;
   @Input() results?: TaskResult[];
+  @Input() gradings?: TaskGrading[];
 
   constructor() {
   }
@@ -19,11 +21,23 @@ export class TaskListComponent implements OnInit {
   ngOnInit() {
   }
 
+  getTaskPoints(taskID: number) {
+    if (this.gradings) {
+      for (let i = this.gradings.length - 1; i >= 0; i--) {
+        const grading = this.gradings[i];
+        if (grading.taskID === taskID) {
+          return grading.points;
+        }
+      }
+    }
+    return this.results[taskID].points;
+  }
+
   getColorClass(taskID: number) {
     if (!this.results) {
       return 'secondary';
     }
-    const points = this.results[taskID].points;
+    const points = this.getTaskPoints(taskID);
     const maxPoints = this.assignment.tasks[taskID].points;
     if (points === maxPoints) {
       return 'success';
