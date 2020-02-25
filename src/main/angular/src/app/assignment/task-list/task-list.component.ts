146906d391f1ce801afe8b0871c359873bd4ea5c
Adrian Kunz <clashsoft@hotmail.com>
2020-02-25 13:51:43 +0100

    Add grade button to task list

--- src/main/angular/src/app/assignment/task-list/task-list.component.ts
+++ src/main/angular/src/app/assignment/task-list/task-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import Assignment from '../model/assignment';
 import TaskResult from '../model/task-result';
+import Solution from '../model/solution';
 
 @Component({
   selector: 'app-task-list',
@@ -9,6 +10,7 @@ import TaskResult from '../model/task-result';
 })
 export class TaskListComponent implements OnInit {
   @Input() assignment?: Assignment;
+  @Input() solution?: Solution;
   @Input() results?: TaskResult[];
 
   constructor() {
