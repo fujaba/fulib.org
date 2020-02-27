90e5899e863e32784df1a99a37ca71fab4605aba
Adrian Kunz <clashsoft@hotmail.com>
2020-02-27 11:18:31 +0100

    Implement course service

--- src/main/angular/src/app/assignment/course.service.ts
+++ src/main/angular/src/app/assignment/course.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import Course from './model/course';
+import {environment} from '../../environments/environment';
+
+type CourseResponse = { id: string, descriptionHtml: string }
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) {
+  }
+
+  public get(id: string): Observable<Course> {
+    return this.http.get<Course>(`${environment.apiURL}/courses/${id}`);
+  }
+
+  public create(course: Course): Observable<Course> {
+    return this.http.post<CourseResponse>(`${environment.apiURL}/courses`, course).pipe(
+      map(response => {
+        return {
+          ...course,
+          ...response,
+        } as Course;
+      })
+    );
+  }
 }
