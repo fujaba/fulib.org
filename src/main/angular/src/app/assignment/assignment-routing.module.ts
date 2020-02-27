d00d9da827aff26ee9d90fadcb12af7fadf07ed4
Adrian Kunz <clashsoft@hotmail.com>
2020-02-27 11:58:20 +0100

    Add router link for course component

--- src/main/angular/src/app/assignment/assignment-routing.module.ts
+++ src/main/angular/src/app/assignment/assignment-routing.module.ts
@@ -4,12 +4,14 @@ import {CreateComponent} from './create/create.component';
 import {SolveComponent} from './solve/solve.component';
 import {SolutionListComponent} from './solution-list/solution-list.component';
 import {SolutionComponent} from './solution/solution.component';
+import {CourseComponent} from './course/course.component';
 
 const routes: Routes = [
   {path: 'create', component: CreateComponent},
   {path: ':id', component: SolveComponent},
   {path: ':id/solutions', component: SolutionListComponent},
   {path: ':aid/solutions/:sid', component: SolutionComponent},
+  {path: 'courses/:cid', component: CourseComponent},
 ];
 
 @NgModule({
