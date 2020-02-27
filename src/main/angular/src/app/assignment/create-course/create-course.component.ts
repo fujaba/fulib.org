471404ba853a5767aa776bd02d33a89277bd060a
Adrian Kunz <clashsoft@hotmail.com>
2020-02-27 14:45:41 +0100

    Start course creation form

--- src/main/angular/src/app/assignment/create-course/create-course.component.ts
+++ src/main/angular/src/app/assignment/create-course/create-course.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import Assignment from '../model/assignment';
+import {AssignmentService} from '../assignment.service';
 
 @Component({
   selector: 'app-create-course',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-course.component.scss']
 })
 export class CreateCourseComponent implements OnInit {
+  title: string;
+  description: string;
+  assignments: Assignment[] = [];
 
-  constructor() { }
+  newAssignment: string;
+
+  constructor(
+    private assignmentService: AssignmentService,
+  ) {
+  }
 
   ngOnInit() {
   }
 
+  saveDraft(): void {
+    // TODO
+  }
+
+  addAssignment() {
+    const newID = this.getNewID();
+    this.assignmentService.get(newID).subscribe(assignment => {
+      this.assignments.push(assignment);
+      this.newAssignment = '';
+    });
+  }
+
+  getNewID(): string {
+    const pattern = /assignments\/([\w-]+)/;
+    const match = pattern.exec(this.newAssignment);
+    if (match) {
+      return match[1];
+    }
+    return this.newAssignment;
+  }
 }
