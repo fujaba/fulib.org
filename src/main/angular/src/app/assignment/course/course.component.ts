f9f1df2bcf964ae821fa0f3b785fb5742edbffe5
Adrian Kunz <clashsoft@hotmail.com>
2020-02-27 12:05:15 +0100

    Start course component

--- src/main/angular/src/app/assignment/course/course.component.ts
+++ src/main/angular/src/app/assignment/course/course.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import Course from '../model/course';
 
 @Component({
   selector: 'app-course',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course.component.scss']
 })
 export class CourseComponent implements OnInit {
+  @Input() course: Course;
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
