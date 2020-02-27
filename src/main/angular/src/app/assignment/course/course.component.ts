19840edfaa2a797cb8d006314b912d132bdab771
Adrian Kunz <clashsoft@hotmail.com>
2020-02-27 12:52:33 +0100

    Load assignments for course

--- src/main/angular/src/app/assignment/course/course.component.ts
+++ src/main/angular/src/app/assignment/course/course.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import Course from '../model/course';
 import {CourseService} from '../course.service';
+import Assignment from '../model/assignment';
+import {AssignmentService} from '../assignment.service';
 
 @Component({
   selector: 'app-course',
@@ -17,6 +19,7 @@ export class CourseComponent implements OnInit {
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
+    private assignmentService: AssignmentService,
     private courseService: CourseService,
   ) { }
 
@@ -28,9 +31,18 @@ export class CourseComponent implements OnInit {
       this.courseService.get(this.courseID).subscribe(course => {
         this.course = course;
 
+        const assignmentIds = course.assignmentIds;
         if (!this.assignmentID) {
-          const firstAssignment = course.assignmentIds[0];
+          const firstAssignment = assignmentIds[0];
           this.router.navigate(['assignments', 'courses', this.courseID, 'assignments', firstAssignment]);
+        }
+
+        course.assignments = new Array<Assignment>(assignmentIds.length);
+        for (let i = 0; i < assignmentIds.length; i++) {
+          const id = assignmentIds[i];
+          this.assignmentService.get(id).subscribe(assignment => {
+            course.assignments[i] = assignment;
+          });
         }
       });
     })
