4277621ef9a550a11292f6af2e73de0de766868b
Adrian Kunz <clashsoft@hotmail.com>
2020-02-28 11:03:23 +0100

    Add dragula polyfill and css (from ng2-dragular README)

--- src/main/angular/src/polyfills.ts
+++ src/main/angular/src/polyfills.ts
@@ -57,6 +57,9 @@
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
+// Dragula
+// from https://github.com/valor-software/ng2-dragula/#1-important-add-the-following-line-to-your-polyfillsts
+(window as any).global = window;
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
