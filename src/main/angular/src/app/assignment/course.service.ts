9bd307441092b589353be20da5977803289ef721
Adrian Kunz <clashsoft@hotmail.com>
2020-03-01 20:15:42 +0100

    Cache courses in course service

--- src/main/angular/src/app/assignment/course.service.ts
+++ src/main/angular/src/app/assignment/course.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {map} from 'rxjs/operators';
 import Course from './model/course';
 import {environment} from '../../environments/environment';
@@ -12,6 +12,8 @@ type CourseResponse = { id: string, descriptionHtml: string }
 })
 export class CourseService {
   private _draft?: Course | null;
+
+  private _cache = new Map<string, Course>();
 
   constructor(
     private http: HttpClient,
@@ -30,23 +32,33 @@ export class CourseService {
     this._draft = value;
     if (value) {
       localStorage.setItem('courseDraft', JSON.stringify(value));
-    }
-    else {
+    } else {
       localStorage.removeItem('courseDraft');
     }
   }
 
   public get(id: string): Observable<Course> {
-    return this.http.get<Course>(`${environment.apiURL}/courses/${id}`);
+    const cached = this._cache.get(id);
+    if (cached) {
+      return of(cached);
+    }
+    return this.http.get<Course>(`${environment.apiURL}/courses/${id}`).pipe(
+      map(response => {
+        this._cache.set(id, response);
+        return response;
+      }),
+    );
   }
 
   public create(course: Course): Observable<Course> {
     return this.http.post<CourseResponse>(`${environment.apiURL}/courses`, course).pipe(
       map(response => {
-        return {
+        const result = {
           ...course,
           ...response,
         } as Course;
+        this._cache.set(course.id, result);
+        return result;
       })
     );
   }
