12bcf3fa6fa7187905578718ee07b5b05142403b
Adrian Kunz <clashsoft@hotmail.com>
2020-03-07 15:23:46 +0100

    Update CollectErrors

--- src/test/java/org/fulib/webapp/mongo/CollectErrors.java
+++ src/test/java/org/fulib/webapp/mongo/CollectErrors.java
@@ -3,7 +3,10 @@ package org.fulib.webapp.mongo;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Projections;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,9 +24,18 @@ public class CollectErrors
 		final Pattern pattern = Pattern.compile("\\[[\\w.]+]");
 		final Map<String, AtomicInteger> counts = new HashMap<>();
 
-		for (final Document document : Mongo.get().requestLog.find(Filters.eq("versions.fulibScenarios", "1.0.0"))
+		final LocalDate start = LocalDate.of(2019, 10, 1);
+		final LocalDateTime end = LocalDateTime.of(2020, 2, 16, 18, 0, 0);
+		final Bson filter = Filters.and(Filters.gte("timestamp", start), Filters.lt("timestamp", end));
+
+		int totalCount = 0;
+		int totalFound = 0;
+
+		for (final Document document : Mongo.get().requestLog.find(filter)
 		                                                     .projection(Projections.include("response.output")))
 		{
+			totalCount++;
+
 			final Document response = document.get("response", Document.class);
 			final String output = response.getString("output");
 			if (output == null)
@@ -31,11 +43,19 @@ public class CollectErrors
 				continue;
 			}
 
+			boolean found = false;
+
 			final Matcher matcher = pattern.matcher(output);
 			while (matcher.find())
 			{
 				final String match = matcher.group();
 				counts.computeIfAbsent(match, k -> new AtomicInteger()).getAndIncrement();
+
+				found = true;
+			}
+
+			if (found) {
+				totalFound++;
 			}
 		}
 
@@ -43,7 +63,10 @@ public class CollectErrors
 		      .stream()
 		      .sorted(Map.Entry.comparingByValue(Comparator.comparing(AtomicInteger::longValue)))
 		      .forEach(entry -> {
-			      System.out.printf("%3d * %20s%n", entry.getValue().get(), entry.getKey());
+			      System.out.printf("%3d * %s%n", entry.getValue().get(), entry.getKey());
 		      });
+
+		System.out.println(totalCount + " total requests");
+		System.out.println(totalFound + " requests with at least one marker");
 	}
 }
