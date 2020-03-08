41e93bb3feea45c29ec212f9d3163f4d8ca69769
Adrian Kunz <clashsoft@hotmail.com>
2020-03-08 18:07:56 +0100

    Implement my solution list
    Closes #76

--- src/main/angular/src/app/assignment/my-solution-list/my-solution-list.component.ts
+++ src/main/angular/src/app/assignment/my-solution-list/my-solution-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import Solution from '../model/solution';
+import Assignment from '../model/assignment';
+import {SolutionService} from '../solution.service';
+import {AssignmentService} from '../assignment.service';
 
 @Component({
   selector: 'app-my-solution-list',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-solution-list.component.scss']
 })
 export class MySolutionListComponent implements OnInit {
+  private _assignments = new Map<string, { assignment: Assignment, solutions: Solution[] }>();
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private solutionService: SolutionService,
+    private assignmentService: AssignmentService,
+  ) {
   }
 
+  get assignments(): Assignment[] {
+    return Array.from(this._assignments.values()).map(a => a.assignment).filter(a => a);
+  }
+
+  getSolutions(assignment: Assignment): Solution[] {
+    return this._assignments.get(assignment.id).solutions;
+  }
+
+  ngOnInit() {
+    for (let ids of this.solutionService.getOwnIds()) {
+      const aid = ids.assignment;
+      const sid = ids.id;
+
+      let holder = this._assignments.get(aid);
+      if (!holder) {
+        holder = {
+          assignment: undefined,
+          solutions: [],
+        };
+        this._assignments.set(aid, holder);
+
+        this.assignmentService.get(aid).subscribe(assignment => holder.assignment = assignment);
+      }
+
+      this.solutionService.get(aid, sid).subscribe(solution => holder.solutions.push(solution));
+    }
+  }
 }
