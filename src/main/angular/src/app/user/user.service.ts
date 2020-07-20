bac31288edd6df9e195c675855dfd3abc592ccc3
Adrian Kunz <clashsoft@hotmail.com>
2020-07-20 22:33:07 +0200

    Add UserService.current$ stub

--- src/main/angular/src/app/user/user.service.ts
+++ src/main/angular/src/app/user/user.service.ts
@@ -1,9 +1,13 @@
 import {Injectable} from '@angular/core';
+import {Observable} from "rxjs";
+import {User} from "./user";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  readonly current$: Observable<User | null>;
+
   constructor() {
   }
 }
