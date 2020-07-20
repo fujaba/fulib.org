8b0d2a7b48dbbeb78dec12dad924fff470a16c0d
Adrian Kunz <clashsoft@hotmail.com>
2020-07-20 22:53:18 +0200

    Implement UserService

--- src/main/angular/src/app/user/user.service.ts
+++ src/main/angular/src/app/user/user.service.ts
@@ -1,13 +1,46 @@
-import {Injectable} from '@angular/core';
-import {Observable} from "rxjs";
+import {Injectable, OnInit} from '@angular/core';
+import {BehaviorSubject, Observable} from "rxjs";
 import {User} from "./user";
+import {KeycloakService} from "keycloak-angular";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  readonly current$: Observable<User | null>;
+  private readonly _current = new BehaviorSubject<User | null>(null);
 
-  constructor() {
+  constructor(
+    private keycloak: KeycloakService,
+  ) {
+    this.init();
+  }
+
+  get current$(): Observable<User | null> {
+    return this._current;
+  }
+
+  private init(): void {
+    this.reload();
+    this.keycloak.keycloakEvents$.subscribe(event => {
+      this.reload();
+    });
+  }
+
+  private reload(): void {
+    this.keycloak.isLoggedIn().then(loggedIn => {
+      if (!loggedIn) {
+        this._current.next(null);
+        return;
+      }
+      this.keycloak.loadUserProfile().then(profile => {
+        const user: User = {
+          id: this.keycloak.getKeycloakInstance().subject,
+          email: profile.email,
+          firstName: profile.firstName,
+          lastName: profile.lastName,
+        };
+        this._current.next(user);
+      });
+    });
   }
 }
