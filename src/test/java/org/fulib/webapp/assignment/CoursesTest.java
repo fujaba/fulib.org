9a2f0cddd5ca93340a23e4ebd62e9d7063991781
Adrian Kunz <clashsoft@hotmail.com>
2020-07-26 13:48:19 +0200

    Add CoursesTest

---
+++ src/test/java/org/fulib/webapp/assignment/CoursesTest.java
@@ -0,0 +1,58 @@
+package org.fulib.webapp.assignment;
+
+import org.fulib.webapp.assignment.model.Course;
+import org.fulib.webapp.mongo.Mongo;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import spark.Request;
+import spark.Response;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.*;
+
+public class CoursesTest
+{
+	private static final String ID = "1";
+	private static final String TITLE = "Test Course";
+	private static final String DESCRIPTION = "A course for the test.";
+	private static final String DESCRIPTION_HTML = "<p>A course for the test.</p>\n";
+
+	@Test
+	public void create()
+	{
+		final Mongo db = mock(Mongo.class);
+		final Courses courses = new Courses(db);
+
+		final Request request = mock(Request.class);
+
+		final JSONObject requestObj = new JSONObject();
+		requestObj.put("title", TITLE);
+		requestObj.put("description", DESCRIPTION);
+
+		requestObj.put("assignmentIds", new JSONArray().put("a1").put("a2"));
+
+		final String requestBody = requestObj.toString();
+		when(request.body()).thenReturn(requestBody);
+
+		final Response response = mock(Response.class);
+
+		final String responseBody = (String) courses.create(request, response);
+		final ArgumentCaptor<Course> courseCaptor = ArgumentCaptor.forClass(Course.class);
+
+		verify(db).saveCourse(courseCaptor.capture());
+
+		final JSONObject responseObj = new JSONObject(responseBody);
+		assertThat(responseObj.getString("id"), notNullValue());
+		assertThat(responseObj.getString("descriptionHtml"), equalTo(DESCRIPTION_HTML));
+
+		final Course course = courseCaptor.getValue();
+		assertThat(course.getId(), notNullValue());
+		assertThat(course.getTitle(), equalTo(TITLE));
+		assertThat(course.getDescription(), equalTo(DESCRIPTION));
+		assertThat(course.getDescriptionHtml(), equalTo(DESCRIPTION_HTML));
+		assertThat(course.getAssignmentIds(), hasItems("a1", "a2"));
+	}
+}
