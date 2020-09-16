0bae32c26fd23260afea8f42be8df414499c8e1c
Adrian Kunz <clashsoft@hotmail.com>
2020-09-16 14:55:07 +0200

    style: Clean up imports in app.module.ts and app-routing.module.ts

--- src/main/angular/src/app/app-routing.module.ts
+++ src/main/angular/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {ChangelogComponent} from './changelog/changelog.component';
-import {ConfigComponent} from './config/config.component';
-import {FeedbackComponent} from './feedback/feedback.component';
 
 import {FourPaneEditorComponent} from './four-pane-editor/four-pane-editor.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {FeedbackComponent} from './feedback/feedback.component';
 import {PrivacyComponent} from './privacy/privacy.component';
+import {ConfigComponent} from './config/config.component';
+import {ChangelogComponent} from './changelog/changelog.component';
 
 const routes: Routes = [
   {path: '', component: FourPaneEditorComponent},
--- src/main/angular/src/app/app.module.ts
+++ src/main/angular/src/app/app.module.ts
@@ -1,25 +1,23 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
-import {SharedModule} from './shared/shared.module';
+import {NgBootstrapDarkmodeModule} from 'ng-bootstrap-darkmode';
 
 import {AppRoutingModule} from './app-routing.module';
 
 import {AppComponent} from './app.component';
-import {FourPaneEditorComponent} from './four-pane-editor/four-pane-editor.component';
-import {ConfigComponent} from './config/config.component';
-import {FooterComponent} from './footer/footer.component';
-
-import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {ChangelogComponent} from './changelog/changelog.component';
-import {NgBootstrapDarkmodeModule} from 'ng-bootstrap-darkmode';
-import {ModalComponent} from './modal/modal.component';
+import {ConfigComponent} from './config/config.component';
 import {FeedbackComponent} from './feedback/feedback.component';
-import { PrivacyComponent } from './privacy/privacy.component';
+import {FooterComponent} from './footer/footer.component';
+import {FourPaneEditorComponent} from './four-pane-editor/four-pane-editor.component';
+import {ModalComponent} from './modal/modal.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {PrivacyComponent} from './privacy/privacy.component';
+import {SharedModule} from './shared/shared.module';
 
 @NgModule({
   declarations: [
