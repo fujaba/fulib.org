57a23e111416f9e1e4259ac9618786808a843996
Adrian Kunz <clashsoft@hotmail.com>
2020-10-17 12:03:48 +0200

    feat: Properly render tables in Markdown

--- src/main/java/org/fulib/webapp/tool/MarkdownUtil.java
+++ src/main/java/org/fulib/webapp/tool/MarkdownUtil.java
@@ -7,6 +7,7 @@ import org.commonmark.ext.gfm.tables.TablesExtension;
 import org.commonmark.ext.task.list.items.TaskListItemsExtension;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.AttributeProvider;
 import org.commonmark.renderer.html.HtmlRenderer;
 
 import java.util.Arrays;
@@ -24,7 +25,15 @@ public class MarkdownUtil
 
 	private static final Parser PARSER = Parser.builder().extensions(EXTENSIONS).build();
 
-	private static final HtmlRenderer RENDERER = HtmlRenderer.builder().extensions(EXTENSIONS).escapeHtml(true).build();
+	private static final AttributeProvider ATTRIBUTE_PROVIDER = (node, tagName, attributes) -> {
+		switch (tagName)
+		{
+		case "table":
+			attributes.put("class", "table table-bordered");
+			return;
+		}
+	};
+	private static final HtmlRenderer RENDERER = HtmlRenderer.builder().extensions(EXTENSIONS).escapeHtml(true).attributeProviderFactory(context -> ATTRIBUTE_PROVIDER).build();
 
 	public static String renderHtml(String markdown)
 	{
