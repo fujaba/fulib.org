2ac6a06edcb80eff56d580ed8c3fc7b737fa6872
Adrian Kunz <clashsoft@hotmail.com>
2020-11-08 13:58:01 +0100

    feat: Add projects module with routing

---
+++ src/main/angular/src/app/projects/projects.module.ts
@@ -0,0 +1,15 @@
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+
+import {ProjectsRoutingModule} from './projects-routing.module';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    ProjectsRoutingModule,
+  ],
+})
+export class ProjectsModule {
+}
