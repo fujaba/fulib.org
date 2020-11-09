6f54e846ff77945fd154715549f7f154831da46e
Adrian Kunz <clashsoft@hotmail.com>
2020-11-10 00:17:56 +0100

    feat: Show project name and description above file tree

--- src/main/angular/src/app/projects/project-tree/project-tree.component.ts
+++ src/main/angular/src/app/projects/project-tree/project-tree.component.ts
@@ -3,6 +3,7 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {FileHandler} from '../file-handler';
 import {File} from '../model/file.interface';
 import {FILE_ROOT} from '../injection-tokens';
+import {Project} from '../model/project';
 
 @Component({
   selector: 'app-project-tree',
@@ -13,6 +14,7 @@ export class ProjectTreeComponent implements OnInit {
   constructor(
     @Inject(FILE_ROOT) public fileRoot: File,
     public fileHandler: FileHandler,
+    public project: Project,
   ) {
   }
 
