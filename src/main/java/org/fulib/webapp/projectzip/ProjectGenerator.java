4f0d02818a8b45551f1654105f30c4cd0e41c3d6
Adrian Kunz <clashsoft@hotmail.com>
2020-11-11 23:03:05 +0100

    refactor: Add ProjectData and ProjectGenerator
    They make it possible to reuse ProjectZip functionality for Projects.

---
+++ src/main/java/org/fulib/webapp/projectzip/ProjectGenerator.java
@@ -0,0 +1,91 @@
+package org.fulib.webapp.projectzip;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class ProjectGenerator
+{
+	interface FileGenerator
+	{
+		void generate(String name, OutputStreamConsumer output) throws IOException;
+	}
+
+	interface OutputStreamConsumer
+	{
+		void accept(OutputStream output) throws IOException;
+	}
+
+	private final byte[] buffer = new byte[8192];
+
+	public void generate(ProjectData data, FileGenerator generator) throws IOException
+	{
+		final String packageDir = data.getPackageName().replace('.', '/');
+		final String fileName = data.getScenarioFileName();
+
+		generator.generate("src/main/scenarios/" + packageDir + "/" + fileName,
+		                   output -> output.write(data.getScenarioText().getBytes(StandardCharsets.UTF_8)));
+
+		copy(generator, "default.gitignore", ".gitignore");
+		copy(generator, "gradlew", "gradlew");
+		copy(generator, "gradlew.bat", "gradlew.bat");
+		copy(generator, "gradle/wrapper/gradle-wrapper.jar.zip", "gradle/wrapper/gradle-wrapper.jar");
+		copy(generator, "gradle/wrapper/gradle-wrapper.properties", "gradle/wrapper/gradle-wrapper.properties");
+
+		generator.generate("settings.gradle",
+		                   output -> output.write(getSettingsGradle(data).getBytes(StandardCharsets.UTF_8)));
+		generator.generate("build.gradle",
+		                   output -> output.write(getBuildGradle(data).getBytes(StandardCharsets.UTF_8)));
+
+		final String decorator = data.getDecoratorClassName();
+		if (decorator != null)
+		{
+			generator.generate("src/gen/java/" + packageDir + "/" + decorator + ".java",
+			                   output -> output.write(getDecoratorJava(data).getBytes(StandardCharsets.UTF_8)));
+		}
+	}
+
+	private String getSettingsGradle(ProjectData data) throws IOException
+	{
+		try (final InputStream input = ProjectZip.class.getResourceAsStream("settings.gradle"))
+		{
+			final String content = IOUtils.toString(input, StandardCharsets.UTF_8);
+			return content.replace("$$projectName$$", data.getProjectName());
+		}
+	}
+
+	private String getBuildGradle(ProjectData data) throws IOException
+	{
+		try (final InputStream input = ProjectZip.class.getResourceAsStream("build.gradle"))
+		{
+			final String content = IOUtils.toString(input, StandardCharsets.UTF_8);
+			return content
+				.replace("$$packageName$$", data.getPackageName())
+				.replace("$$projectVersion$$", data.getProjectVersion());
+		}
+	}
+
+	private String getDecoratorJava(ProjectData data) throws IOException
+	{
+		try (final InputStream input = ProjectZip.class.getResourceAsStream("Decorator.java.txt"))
+		{
+			final String content = IOUtils.toString(input, StandardCharsets.UTF_8);
+			return content
+				.replace("$$packageName$$", data.getPackageName())
+				.replace("$$decoratorClassName$$", data.getDecoratorClassName());
+		}
+	}
+
+	private void copy(FileGenerator generator, String resourceName, String file) throws IOException
+	{
+		generator.generate(file, output -> {
+			try (final InputStream fileInput = ProjectZip.class.getResourceAsStream(resourceName))
+			{
+				IOUtils.copyLarge(fileInput, output, buffer);
+			}
+		});
+	}
+}
--- src/main/java/org/fulib/webapp/projectzip/ProjectZip.java
+++ src/main/java/org/fulib/webapp/projectzip/ProjectZip.java
@@ -1,6 +1,5 @@
 package org.fulib.webapp.projectzip;
 
-import org.apache.commons.io.IOUtils;
 import org.fulib.webapp.mongo.Mongo;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -8,8 +7,6 @@ import spark.Request;
 import spark.Response;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -26,57 +23,15 @@ public class ProjectZip
 	{
 		final String body = request.body();
 		final JSONObject jsonObject = new JSONObject(body);
-		final String packageName = jsonObject.getString("packageName");
-		final String fileName = jsonObject.getString("scenarioFileName");
-		final String projectName = jsonObject.getString("projectName");
-		final String projectVersion = jsonObject.getString("projectVersion");
-		final String bodyText = jsonObject.getString("scenarioText");
-		final String decoratorClassName = jsonObject.optString("decoratorClassName", null);
-
-		final String packageDir = packageName.replace('.', '/');
+		final ProjectData projectData = readJson(jsonObject);
 
 		response.type("application/zip");
 		try (final ZipOutputStream zip = new ZipOutputStream(response.raw().getOutputStream()))
 		{
-			final byte[] buffer = new byte[8192];
-
-			zip.putNextEntry(new ZipEntry("src/main/scenarios/" + packageDir + "/" + fileName));
-			zip.write(bodyText.getBytes(StandardCharsets.UTF_8));
-
-			copy("default.gitignore", ".gitignore", zip, buffer);
-			copy("gradlew", "gradlew", zip, buffer);
-			copy("gradlew.bat", "gradlew.bat", zip, buffer);
-			copy("gradle/wrapper/gradle-wrapper.jar.zip", "gradle/wrapper/gradle-wrapper.jar", zip, buffer);
-			copy("gradle/wrapper/gradle-wrapper.properties", "gradle/wrapper/gradle-wrapper.properties", zip, buffer);
-
-			zip.putNextEntry(new ZipEntry("settings.gradle"));
-			try (final InputStream input = ProjectZip.class.getResourceAsStream("settings.gradle"))
-			{
-				final String content = IOUtils.toString(input, StandardCharsets.UTF_8);
-				final String result = content.replace("$$projectName$$", projectName);
-				IOUtils.write(result, zip, StandardCharsets.UTF_8);
-			}
-
-			zip.putNextEntry(new ZipEntry("build.gradle"));
-			try (final InputStream input = ProjectZip.class.getResourceAsStream("build.gradle"))
-			{
-				final String content = IOUtils.toString(input, StandardCharsets.UTF_8);
-				final String result = content.replace("$$packageName$$", packageName)
-				                             .replace("$$projectVersion$$", projectVersion);
-				IOUtils.write(result, zip, StandardCharsets.UTF_8);
-			}
-
-			if (decoratorClassName != null && !decoratorClassName.isEmpty())
-			{
-				zip.putNextEntry(new ZipEntry("src/gen/java/" + packageDir + "/" + decoratorClassName + ".java"));
-				try (final InputStream input = ProjectZip.class.getResourceAsStream("Decorator.java.txt"))
-				{
-					final String content = IOUtils.toString(input, StandardCharsets.UTF_8);
-					final String result = content.replace("$$packageName$$", packageName)
-					                             .replace("$$decoratorClassName$$", decoratorClassName);
-					IOUtils.write(result, zip, StandardCharsets.UTF_8);
-				}
-			}
+			new ProjectGenerator().generate(projectData, (name, output) -> {
+				zip.putNextEntry(new ZipEntry(name));
+				output.accept(zip);
+			});
 		}
 
 		if (jsonObject.has("privacy") && "all".equals(jsonObject.get("privacy")))
@@ -87,12 +42,15 @@ public class ProjectZip
 		return response.raw();
 	}
 
-	private static void copy(String resourceName, String file, ZipOutputStream zip, byte[] buffer) throws IOException
+	private static ProjectData readJson(JSONObject json)
 	{
-		zip.putNextEntry(new ZipEntry(file));
-		try (final InputStream fileInput = ProjectZip.class.getResourceAsStream(resourceName))
-		{
-			IOUtils.copyLarge(fileInput, zip, buffer);
-		}
+		final ProjectData projectData = new ProjectData();
+		projectData.setPackageName(json.getString(ProjectData.PROPERTY_PACKAGE_NAME));
+		projectData.setScenarioFileName(json.getString(ProjectData.PROPERTY_SCENARIO_FILE_NAME));
+		projectData.setProjectName(json.getString(ProjectData.PROPERTY_PROJECT_NAME));
+		projectData.setProjectVersion(json.getString(ProjectData.PROPERTY_PROJECT_VERSION));
+		projectData.setScenarioText(json.getString(ProjectData.PROPERTY_SCENARIO_TEXT));
+		projectData.setDecoratorClassName(json.optString(ProjectData.PROPERTY_DECORATOR_CLASS_NAME, null));
+		return projectData;
 	}
 }
