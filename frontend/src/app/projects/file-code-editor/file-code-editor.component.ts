3ad3b8faed47642d397fa4a68c60fc05d52a4729
Adrian Kunz <clashsoft@hotmail.com>
2020-11-11 22:22:30 +0100

    feat: Use codemirror mode depending on file type

--- frontend/src/app/projects/file-code-editor/file-code-editor.component.ts
+++ frontend/src/app/projects/file-code-editor/file-code-editor.component.ts
@@ -1,4 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {filter} from 'rxjs/operators';
+import {FileTypeService} from '../file-type.service';
 import {FileManager} from '../file.manager';
 import {File} from '../model/file';
 
@@ -10,16 +13,40 @@ import {File} from '../model/file';
 export class FileCodeEditorComponent implements OnInit {
   @Input() file: File;
 
+  subscription: Subscription;
+
+  options = {
+    mode: 'null',
+    lineNumbers: true,
+    lineWrapping: true,
+    styleActiveLine: true,
+    extraKeys: {
+      'Ctrl-S': this.save.bind(this),
+      'Cmd-S': this.save.bind(this),
+    },
+    autoRefresh: true,
+  };
+
   constructor(
     private fileManager: FileManager,
+    private fileTypeService: FileTypeService,
   ) {
   }
 
   ngOnInit(): void {
+    this.updateFileType();
+    this.subscription = this.fileManager.updates.pipe(filter(file => file === this.file)).subscribe(() => {
+      this.updateFileType();
+    });
+  }
+
+  private updateFileType() {
+    this.options.mode = this.fileTypeService.getFileType(this.file).mode;
   }
 
   save() {
-    this.fileManager.saveContent(this.file).subscribe(() => {});
+    this.fileManager.saveContent(this.file).subscribe(() => {
+    });
   }
 
   setContent(content: string) {
