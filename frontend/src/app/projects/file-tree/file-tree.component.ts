02f4766a31fa7dc6eb78d3feab62a377a06cff1d
Adrian Kunz <clashsoft@hotmail.com>
2020-11-15 14:48:19 +0100

    fix: Automatically focus name input when renaming

--- frontend/src/app/projects/file-tree/file-tree.component.ts
+++ frontend/src/app/projects/file-tree/file-tree.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {NgbDropdown} from '@ng-bootstrap/ng-bootstrap';
 import {Observable} from 'rxjs';
 import {FileManager} from '../file.manager';
@@ -12,9 +12,11 @@ import {File} from '../model/file';
   templateUrl: './file-tree.component.html',
   styleUrls: ['./file-tree.component.scss'],
 })
-export class FileTreeComponent implements OnInit {
+export class FileTreeComponent implements OnInit, AfterViewInit {
   @Input() root: File;
   @Input() level = 0;
+
+  @ViewChildren('nameInput') nameInput: QueryList<ElementRef>
 
   expanded = false;
   oldName?: string;
@@ -27,6 +29,12 @@ export class FileTreeComponent implements OnInit {
 
   ngOnInit(): void {
     this.currentFile = this.fileManager.currentFile;
+  }
+
+  ngAfterViewInit(): void {
+    this.nameInput.changes.subscribe(() => {
+      this.nameInput.first?.nativeElement.focus();
+    });
   }
 
   open() {
