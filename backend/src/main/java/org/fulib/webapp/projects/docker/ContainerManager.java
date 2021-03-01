0d8fef8b47196e28786f451c3d44390e0d5264a8
Adrian Kunz <clashsoft@hotmail.com>
2021-03-01 11:09:13 +0100

    fix(backend): Ignore NotFoundException when stopping container

--- backend/src/main/java/org/fulib/webapp/projects/docker/ContainerManager.java
+++ backend/src/main/java/org/fulib/webapp/projects/docker/ContainerManager.java
@@ -4,6 +4,7 @@ import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.async.ResultCallback;
 import com.github.dockerjava.api.command.CreateContainerCmd;
 import com.github.dockerjava.api.command.InspectContainerResponse;
+import com.github.dockerjava.api.exception.NotFoundException;
 import com.github.dockerjava.api.model.ExposedPort;
 import com.github.dockerjava.api.model.NetworkSettings;
 import com.github.dockerjava.api.model.Ports;
@@ -145,8 +146,14 @@ public class ContainerManager
 
 	public void stop(Container container)
 	{
-		this.dockerClient.stopContainerCmd(container.getId()).exec();
-		this.dockerClient.removeContainerCmd(container.getId()).exec();
+		try
+		{
+			this.dockerClient.stopContainerCmd(container.getId()).exec();
+			this.dockerClient.removeContainerCmd(container.getId()).exec();
+		}
+		catch (NotFoundException ignored)
+		{
+		}
 	}
 
 	public void uploadFilesFromContainer(Container container)
