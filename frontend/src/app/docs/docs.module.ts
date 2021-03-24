0e61c81344a112a1a356d59c9da7c06faa88906c
Adrian Kunz <clashsoft@hotmail.com>
2021-03-24 15:51:25 +0100

    feat(frontend): Add docs module, routing and header link

---
+++ frontend/src/app/docs/docs.module.ts
@@ -0,0 +1,15 @@
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+
+import {DocsRoutingModule} from './docs-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DocsRoutingModule,
+  ],
+})
+export class DocsModule {
+}
