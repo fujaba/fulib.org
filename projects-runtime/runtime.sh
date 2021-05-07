53a3dfbb3a824eb9be59e82656856bdcae7ce9d4
Adrian Kunz <clashsoft@hotmail.com>
2021-05-07 11:17:16 +0200

    fix(projects-runtime): Login to registry

--- projects-runtime/runtime.sh
+++ projects-runtime/runtime.sh
@@ -4,6 +4,11 @@ dockerd --host tcp://0.0.0.0:2376 --tls=false & sleep 20
 export DOCKER_HOST=localhost:2376
 docker network create fulib-projects
 
+if [ -n "$DOCKER_REGISTRY" ]
+then
+	docker login "$DOCKER_REGISTRY" -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+fi
+
 docker pull "$FULIB_PROJECTS_CONTAINER_IMAGE":latest &
 docker run -p 8080:80 --network=fulib-projects "$FULIB_PROJECTS_PROXY_IMAGE" &
 nginx -g 'daemon off;' &
