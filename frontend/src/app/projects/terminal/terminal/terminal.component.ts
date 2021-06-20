b431a0d7c1cee158727e44f0a56ba91fa4c976fa
Adrian Kunz <clashsoft@hotmail.com>
2021-06-20 12:59:01 +0200

    feat(frontend): File links in terminal

--- frontend/src/app/projects/terminal/terminal/terminal.component.ts
+++ frontend/src/app/projects/terminal/terminal/terminal.component.ts
@@ -1,8 +1,11 @@
-import {AfterViewInit, Component, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, Input, NgZone, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {NgTerminal} from 'ng-terminal';
 import {Observable} from 'rxjs';
 import {filter, map} from 'rxjs/operators';
+import {IDisposable, ILink} from 'xterm';
+import {FileService} from '../../file.service';
 import {Terminal} from '../../model/terminal';
+import {ProjectManager} from '../../project.manager';
 import {TerminalService} from '../terminal.service';
 
 @Component({
@@ -18,10 +21,14 @@ export class TerminalComponent implements OnInit, OnDestroy, AfterViewInit {
 
   output$: Observable<string>;
 
-  private resize;
+  private resize?: IDisposable;
+  private linkProvider?: IDisposable;
 
   constructor(
     private terminalService: TerminalService,
+    private projectManager: ProjectManager,
+    private fileService: FileService,
+    private zone: NgZone,
   ) {
   }
 
@@ -50,6 +57,17 @@ export class TerminalComponent implements OnInit, OnDestroy, AfterViewInit {
     });
 
     const xterm = this.terminal.underlying;
+    this.linkProvider = xterm.registerLinkProvider({
+      provideLinks: (bufferLineNumber: number, callback: (links: (ILink[] | undefined)) => void) => {
+        const line = xterm.buffer.active.getLine(bufferLineNumber - 1)?.translateToString(true);
+        if (!line) {
+          callback(undefined);
+          return;
+        }
+        const links = this.findLinks(line, bufferLineNumber);
+        callback(links);
+      },
+    });
     this.doResize(xterm.cols, xterm.rows);
     this.resize = xterm.onResize(({cols, rows}) => this.doResize(cols, rows));
   }
@@ -58,7 +76,42 @@ export class TerminalComponent implements OnInit, OnDestroy, AfterViewInit {
     this.terminalService.resize(this.model.id, cols, rows);
   }
 
+  private findLinks(line: string, y: number) {
+    const pattern = /(\/projects\/(?:.*))(?::(\d+)(?::(\d+))?)?/g;
+    const links: ILink[] = [];
+    for (let match = pattern.exec(line); match !== null; match = pattern.exec(line)) {
+      const [text, path, row, column] = match;
+      links.push({
+        text,
+        range: {
+          start: {x: match.index + 1, y},
+          end: {x: match.index + text.length, y},
+        },
+        decorations: {
+          pointerCursor: true,
+          underline: true,
+        },
+        activate: () => this.zone.run(() => this.openEditor(path, row, column)),
+      });
+    }
+    return links;
+  }
+
+  private openEditor(path: string, row: string, column: string) {
+    this.fileService.resolveAsync(this.projectManager.container, this.projectManager.fileRoot, path).subscribe(file => {
+      if (!file) {
+        return;
+      }
+
+      this.projectManager.openEditor({
+        file,
+        temporary: true,
+      });
+    });
+  }
+
   ngOnDestroy(): void {
     this.resize?.dispose();
+    this.linkProvider?.dispose();
   }
 }
