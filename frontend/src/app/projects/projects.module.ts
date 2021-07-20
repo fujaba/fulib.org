a3025907066232defe03d09c1cbfa49bbbdcf550
Adrian Kunz <clashsoft@hotmail.com>
2021-07-20 11:48:33 +0200

    fix(frontend): Remove unnecessary HttpClientModule import
    Fixes a problem where it was no longer possible to receive user-related
    data due to missing bearer token.

--- frontend/src/app/projects/projects.module.ts
+++ frontend/src/app/projects/projects.module.ts
@@ -1,5 +1,4 @@
 import {CommonModule} from '@angular/common';
-import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
@@ -7,22 +6,22 @@ import {AngularSplitModule} from 'angular-split';
 import {DndModule} from 'ngx-drag-drop';
 
 import {SharedModule} from '../shared/shared.module';
+import {FileTabsComponent} from './components/file-tabs/file-tabs.component';
+import {ProjectListComponent} from './components/project-list/project-list.component';
+import {ProjectWorkspaceComponent} from './components/project-workspace/project-workspace.component';
+import {SplitPanelComponent} from './components/split-panel/split-panel.component';
+import {EditorModule} from './modules/editor/editor.module';
+import {SetupModule} from './modules/setup/setup.module';
+import {TerminalModule} from './modules/terminal/terminal.module';
+import {ProjectsRoutingModule} from './projects-routing.module';
 import {ConfigService} from './services/config.service';
 import {ContainerService} from './services/container.service';
 import {DavClient} from './services/dav-client';
-import {EditorModule} from './modules/editor/editor.module';
 import {FileChangeService} from './services/file-change.service';
-import {FileTabsComponent} from './components/file-tabs/file-tabs.component';
 import {FileTypeService} from './services/file-type.service';
 import {FileService} from './services/file.service';
 import {LocalProjectService} from './services/local-project.service';
-import {ProjectListComponent} from './components/project-list/project-list.component';
-import {ProjectWorkspaceComponent} from './components/project-workspace/project-workspace.component';
 import {ProjectService} from './services/project.service';
-import {ProjectsRoutingModule} from './projects-routing.module';
-import {SetupModule} from './modules/setup/setup.module';
-import {SplitPanelComponent} from './components/split-panel/split-panel.component';
-import {TerminalModule} from './modules/terminal/terminal.module';
 
 @NgModule({
   declarations: [
@@ -35,7 +34,6 @@ import {TerminalModule} from './modules/terminal/terminal.module';
     // Angular
     CommonModule,
     FormsModule,
-    HttpClientModule,
     // 3rd Party
     DndModule,
     NgbModule,
