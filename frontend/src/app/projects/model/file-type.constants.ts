3e614b84a54784dd2d84a1cc775d340d12124afc
Adrian Kunz <clashsoft@hotmail.com>
2021-07-21 17:32:42 +0200

    refactor(frontend): Add file-type.constants.ts

---
+++ frontend/src/app/projects/model/file-type.constants.ts
@@ -0,0 +1,106 @@
+import {FileType} from './file-type';
+
+export const DEFAULT: FileType = {
+  name: 'File',
+  extensions: [],
+  icon: 'file-earmark',
+  mode: 'download',
+};
+
+export const FILE_TYPES: Record<string, FileType> = {
+  scenario: {
+    name: 'Scenario',
+    extensions: ['.md'],
+    pathPattern: /scenarios\/.*\.md/,
+    icon: 'markdown',
+    mode: 'scenario',
+    previewMode: 'markdown',
+  },
+  markdown: {
+    name: 'Markdown',
+    extensions: ['.md'],
+    icon: 'markdown',
+    mode: 'text/x-markdown',
+    previewMode: 'markdown',
+  },
+  png: {
+    name: 'PNG Image',
+    extensions: ['.png'],
+    icon: 'image',
+    mode: 'img',
+  },
+  svg: {
+    name: 'SVG Image',
+    extensions: ['.svg'],
+    icon: 'image',
+    mode: 'text/xml',
+    previewMode: 'img',
+  },
+  html: {
+    name: 'HTML',
+    extensions: ['.htm', '.html'],
+    icon: 'file-earmark-richtext',
+    mode: 'text/html',
+    previewMode: 'iframe',
+  },
+  gradle: {
+    name: 'Gradle',
+    extensions: ['.gradle'],
+    icon: 'gear',
+    mode: 'text/x-groovy',
+  },
+  java: {
+    name: 'Java',
+    extensions: ['.java'],
+    icon: 'file-earmark-code',
+    mode: 'text/x-java',
+  },
+  javascript: {
+    name: 'JavaScript',
+    extensions: ['.js', '.jsx'],
+    icon: 'file-earmark-code',
+    mode: 'text/javascript',
+  },
+  typescript: {
+    name: 'TypeScript',
+    extensions: ['.ts', '.tsx'],
+    icon: 'file-earmark-code',
+    mode: 'text/typescript',
+  },
+  properties: {
+    name: 'Properties',
+    extensions: ['.properties'],
+    icon: 'file-earmark-code',
+    mode: 'text/x-properties',
+  },
+  yaml: {
+    name: 'YAML',
+    extensions: ['.yaml', '.yml'],
+    icon: 'file-earmark-code',
+    mode: 'text/x-yaml',
+  },
+  json: {
+    name: 'JSON',
+    extensions: ['.json'],
+    icon: 'file-earmark-code',
+    mode: 'application/json',
+  },
+  archive: {
+    name: 'Archive',
+    extensions: ['.zip', '.jar'],
+    icon: 'file-zip',
+    mode: 'download',
+  },
+  script: {
+    name: 'Script',
+    extensions: ['gradlew', 'gradlew.bat'],
+    icon: 'terminal',
+    mode: 'text/x-sh',
+  },
+  ignore: {
+    name: 'Ignore',
+    extensions: ['.gitignore'],
+    icon: 'file-earmark-text',
+    mode: 'text/plain',
+  },
+};
--- frontend/src/app/projects/services/file-type.service.ts
+++ frontend/src/app/projects/services/file-type.service.ts
@@ -1,113 +1,12 @@
 import {Injectable} from '@angular/core';
 import {File} from '../model/file';
 import {FileType} from '../model/file-type';
+import {DEFAULT, FILE_TYPES} from '../model/file-type.constants';
 
 @Injectable()
 export class FileTypeService {
-  readonly fileTypes: Record<string, FileType> = {
-    scenario: {
-      name: 'Scenario',
-      extensions: ['.md'],
-      pathPattern: /scenarios\/.*\.md/,
-      icon: 'markdown',
-      mode: 'scenario',
-      previewMode: 'markdown',
-    },
-    markdown: {
-      name: 'Markdown',
-      extensions: ['.md'],
-      icon: 'markdown',
-      mode: 'text/x-markdown',
-      previewMode: 'markdown',
-    },
-    png: {
-      name: 'PNG Image',
-      extensions: ['.png'],
-      icon: 'image',
-      mode: 'img',
-    },
-    svg: {
-      name: 'SVG Image',
-      extensions: ['.svg'],
-      icon: 'image',
-      mode: 'text/xml',
-      previewMode: 'img',
-    },
-    html: {
-      name: 'HTML',
-      extensions: ['.htm', '.html'],
-      icon: 'file-earmark-richtext',
-      mode: 'text/html',
-      previewMode: 'iframe',
-    },
-    gradle: {
-      name: 'Gradle',
-      extensions: ['.gradle'],
-      icon: 'gear',
-      mode: 'text/x-groovy',
-    },
-    java: {
-      name: 'Java',
-      extensions: ['.java'],
-      icon: 'file-earmark-code',
-      mode: 'text/x-java',
-    },
-    javascript: {
-      name: 'JavaScript',
-      extensions: ['.js', '.jsx'],
-      icon: 'file-earmark-code',
-      mode: 'text/javascript',
-    },
-    typescript: {
-      name: 'TypeScript',
-      extensions: ['.ts', '.tsx'],
-      icon: 'file-earmark-code',
-      mode: 'text/typescript',
-    },
-    properties: {
-      name: 'Properties',
-      extensions: ['.properties'],
-      icon: 'file-earmark-code',
-      mode: 'text/x-properties',
-    },
-    yaml: {
-      name: 'YAML',
-      extensions: ['.yaml', '.yml'],
-      icon: 'file-earmark-code',
-      mode: 'text/x-yaml',
-    },
-    json: {
-      name: 'JSON',
-      extensions: ['.json'],
-      icon: 'file-earmark-code',
-      mode: 'application/json',
-    },
-    archive: {
-      name: 'Archive',
-      extensions: ['.zip', '.jar'],
-      icon: 'file-zip',
-      mode: 'download',
-    },
-    script: {
-      name: 'Script',
-      extensions: ['gradlew', 'gradlew.bat'],
-      icon: 'terminal',
-      mode: 'text/x-sh',
-    },
-    ignore: {
-      name: 'Ignore',
-      extensions: ['.gitignore'],
-      icon: 'file-earmark-text',
-      mode: 'text/plain',
-    },
-  };
-
-  readonly default: FileType = {
-    name: 'File',
-    extensions: [],
-    icon: 'file-earmark',
-    mode: 'download',
-  };
+  readonly fileTypes = FILE_TYPES;
+  readonly default = DEFAULT;
 
   getFileType(file: File): FileType {
     const name = file.name;
