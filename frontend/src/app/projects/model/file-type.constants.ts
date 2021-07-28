944810280accdf3a6fee51c5388a8d6a98c82c41
Adrian Kunz <clashsoft@hotmail.com>
2021-07-28 19:44:54 +0200

    refactor(frontend): Sort file types alphabetically

--- frontend/src/app/projects/model/file-type.constants.ts
+++ frontend/src/app/projects/model/file-type.constants.ts
@@ -8,19 +8,48 @@ export const DEFAULT: FileType = {
 };
 
 export const FILE_TYPES: Record<string, FileType> = {
-  text: {
-    name: 'Plain Text',
-    extensions: ['.txt'],
-    icon: 'file-earmark',
+  archive: {
+    name: 'Archive',
+    extensions: ['.zip', '.jar'],
+    icon: 'file-zip',
+    mode: 'download',
+  },
+  gradle: {
+    name: 'Gradle',
+    extensions: ['.gradle'],
+    icon: 'gear',
+    mode: 'text/x-groovy',
+  },
+  html: {
+    name: 'HTML',
+    extensions: ['.htm', '.html'],
+    icon: 'file-earmark-richtext',
+    mode: 'text/html',
+    previewMode: 'iframe',
+  },
+  ignore: {
+    name: 'Ignore',
+    extensions: ['.gitignore'],
+    icon: 'file-earmark-text',
     mode: 'text/plain',
   },
-  scenario: {
-    name: 'Scenario',
-    extensions: ['.md'],
-    pathPattern: /scenarios\/.*\.md/,
-    icon: 'markdown',
-    mode: 'scenario',
-    previewMode: 'markdown',
+  java: {
+    name: 'Java',
+    extensions: ['.java'],
+    icon: 'file-earmark-code',
+    mode: 'text/x-java',
+  },
+  json: {
+    name: 'JSON',
+    extensions: ['.json'],
+    icon: 'file-earmark-code',
+    mode: 'application/json',
+  },
+  javascript: {
+    name: 'JavaScript',
+    extensions: ['.js', '.jsx'],
+    icon: 'file-earmark-code',
+    mode: 'text/javascript',
   },
   markdown: {
     name: 'Markdown',
@@ -35,67 +64,19 @@ export const FILE_TYPES: Record<string, FileType> = {
     icon: 'image',
     mode: 'img',
   },
-  svg: {
-    name: 'SVG Image',
-    extensions: ['.svg'],
-    icon: 'image',
-    mode: 'text/xml',
-    previewMode: 'img',
-  },
-  html: {
-    name: 'HTML',
-    extensions: ['.htm', '.html'],
-    icon: 'file-earmark-richtext',
-    mode: 'text/html',
-    previewMode: 'iframe',
-  },
-  gradle: {
-    name: 'Gradle',
-    extensions: ['.gradle'],
-    icon: 'gear',
-    mode: 'text/x-groovy',
-  },
-  java: {
-    name: 'Java',
-    extensions: ['.java'],
-    icon: 'file-earmark-code',
-    mode: 'text/x-java',
-  },
-  javascript: {
-    name: 'JavaScript',
-    extensions: ['.js', '.jsx'],
-    icon: 'file-earmark-code',
-    mode: 'text/javascript',
-  },
-  typescript: {
-    name: 'TypeScript',
-    extensions: ['.ts', '.tsx'],
-    icon: 'file-earmark-code',
-    mode: 'text/typescript',
-  },
   properties: {
     name: 'Properties',
     extensions: ['.properties'],
     icon: 'file-earmark-code',
     mode: 'text/x-properties',
   },
-  yaml: {
-    name: 'YAML',
-    extensions: ['.yaml', '.yml'],
-    icon: 'file-earmark-code',
-    mode: 'text/x-yaml',
-  },
-  json: {
-    name: 'JSON',
-    extensions: ['.json'],
-    icon: 'file-earmark-code',
-    mode: 'application/json',
-  },
-  archive: {
-    name: 'Archive',
-    extensions: ['.zip', '.jar'],
-    icon: 'file-zip',
-    mode: 'download',
+  scenario: {
+    name: 'Scenario',
+    extensions: ['.md'],
+    pathPattern: /scenarios\/.*\.md/,
+    icon: 'markdown',
+    mode: 'scenario',
+    previewMode: 'markdown',
   },
   script: {
     name: 'Script',
@@ -103,10 +84,29 @@ export const FILE_TYPES: Record<string, FileType> = {
     icon: 'terminal',
     mode: 'text/x-sh',
   },
-  ignore: {
-    name: 'Ignore',
-    extensions: ['.gitignore'],
-    icon: 'file-earmark-text',
+  svg: {
+    name: 'SVG Image',
+    extensions: ['.svg'],
+    icon: 'image',
+    mode: 'text/xml',
+    previewMode: 'img',
+  },
+  text: {
+    name: 'Plain Text',
+    extensions: ['.txt'],
+    icon: 'file-earmark',
     mode: 'text/plain',
+  },
+  typescript: {
+    name: 'TypeScript',
+    extensions: ['.ts', '.tsx'],
+    icon: 'file-earmark-code',
+    mode: 'text/typescript',
+  },
+  yaml: {
+    name: 'YAML',
+    extensions: ['.yaml', '.yml'],
+    icon: 'file-earmark-code',
+    mode: 'text/x-yaml',
   },
 };
