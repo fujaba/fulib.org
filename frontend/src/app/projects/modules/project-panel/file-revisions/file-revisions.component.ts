9cb000c31666d7bfc1f259f99f9a3571d87bd5d7
Adrian Kunz <clashsoft@hotmail.com>
2021-08-02 21:10:49 +0200

    fix(frontend): Resolve template type errors

--- frontend/src/app/projects/modules/project-panel/file-revisions/file-revisions.component.ts
+++ frontend/src/app/projects/modules/project-panel/file-revisions/file-revisions.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {File} from '../../../model/file';
+import {Revision} from '../../../model/revision';
 import {ProjectManager} from '../../../services/project.manager';
 
 @Component({
@@ -9,6 +10,7 @@ import {ProjectManager} from '../../../services/project.manager';
 })
 export class FileRevisionsComponent implements OnInit {
   @Input() file?: File;
+  revisions: Revision[] = [];
 
   constructor(
     private projectManager: ProjectManager,
