651a0470f1abb1ade8f0a82b937165c5786616b8
Adrian Kunz <clashsoft@hotmail.com>
2021-08-27 17:05:07 +0200

    feat(frontend): Open Scenario file in editor after Setup

--- frontend/src/app/projects/modules/setup/setup/setup.component.ts
+++ frontend/src/app/projects/modules/setup/setup/setup.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
+import {switchMap} from 'rxjs/operators';
 import {ProjectConfig} from '../../../../model/project-config';
+import {FileService} from '../../../services/file.service';
 import {ProjectManager} from '../../../services/project.manager';
 import {ProjectService} from '../../../services/project.service';
 import {SetupService} from '../setup.service';
@@ -19,6 +21,7 @@ export class SetupComponent implements OnInit {
     private projectService: ProjectService,
     private projectManager: ProjectManager,
     private setupService: SetupService,
+    private fileService: FileService,
   ) {
   }
 
@@ -28,6 +31,21 @@ export class SetupComponent implements OnInit {
 
   save(): void {
     this.projectService.saveConfig(this.projectManager.project, this.config);
-    this.projectService.generateFiles(this.projectManager.container, this.config).subscribe();
+    this.projectService.generateFiles(this.projectManager.container, this.config).pipe(
+      switchMap(() => {
+        const {container, fileRoot} = this.projectManager;
+        const path = `${fileRoot.path}src/main/scenarios/${this.config.packageName.replace('.', '/')}/${this.config.scenarioFileName}`;
+        return this.fileService.resolveAsync(container, fileRoot, path);
+      }),
+    ).subscribe(file => {
+      if (!file) {
+        return;
+      }
+      this.projectManager.openEditor({
+        file,
+        preview: false,
+        temporary: false,
+      });
+    });
   }
 }
