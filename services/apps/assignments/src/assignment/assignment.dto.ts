037ebe2c4047af929f6200ec33627077099db138
Adrian Kunz <clashsoft@hotmail.com>
2021-08-31 14:16:18 +0200

    fix(assignments-service): Make UpdateAssignmentDto partial

--- services/apps/assignments/src/assignment/assignment.dto.ts
+++ services/apps/assignments/src/assignment/assignment.dto.ts
@@ -1,8 +1,8 @@
-import {OmitType} from '@nestjs/swagger';
+import {OmitType, PartialType} from '@nestjs/swagger';
 import {Assignment} from './assignment.schema';
 
 export class CreateAssignmentDto extends OmitType(Assignment, [] as const) {
 }
 
-export class UpdateAssignmentDto extends OmitType(Assignment, [] as const) {
+export class UpdateAssignmentDto extends PartialType(OmitType(Assignment, [] as const)) {
 }
