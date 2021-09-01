1401f5d8e4a183e09777ba673e815ade0d68e957
Adrian Kunz <clashsoft@hotmail.com>
2021-09-01 14:22:51 +0200

    feat(assignments-service): Token authentication for update and delete

--- services/apps/assignments/src/assignment/assignment.controller.ts
+++ services/apps/assignments/src/assignment/assignment.controller.ts
@@ -1,7 +1,8 @@
 import {Auth, AuthUser, UserToken} from '@app/keycloak-auth';
-import {Body, Controller, Delete, Get, Headers, Param, Patch, Post} from '@nestjs/common';
+import {Body, Controller, Delete, ForbiddenException, Get, Headers, Param, Patch, Post} from '@nestjs/common';
 import {
   ApiCreatedResponse,
+  ApiForbiddenResponse,
   ApiHeader,
   ApiNotFoundResponse,
   ApiOkResponse,
@@ -12,6 +13,8 @@ import {notFound} from '../utils';
 import {CreateAssignmentDto, ReadAssignmentDto, UpdateAssignmentDto} from './assignment.dto';
 import {Assignment} from './assignment.schema';
 import {AssignmentService} from './assignment.service';
+
+const forbiddenResponse = 'Not owner or invalid Assignment-Token.';
 
 @Controller('assignments')
 @ApiTags('Assignments')
@@ -45,7 +48,7 @@ export class AssignmentController {
     },
   })
   @ApiNotFoundResponse()
-  @ApiHeader({ name: 'assignment-token', required: false })
+  @ApiHeader({name: 'assignment-token', required: false})
   async findOne(
     @Param('id') id: string,
     @Headers('assignment-token') token?: string,
@@ -56,23 +59,54 @@ export class AssignmentController {
     if (!assignment) {
       notFound(id);
     }
-    if (assignment.token === token || user && user.sub === assignment.userId) {
+    if (this.isAuthorized(assignment, token, user)) {
       return assignment;
     }
     return this.assignmentService.mask(assignment.toObject());
   }
 
   @Patch(':id')
+  @Auth({optional: true})
   @ApiOkResponse({type: Assignment})
   @ApiNotFoundResponse()
-  async update(@Param('id') id: string, @Body() dto: UpdateAssignmentDto) {
+  @ApiForbiddenResponse({description: forbiddenResponse})
+  @ApiHeader({name: 'assignment-token', required: false})
+  async update(
+    @Param('id') id: string,
+    @Body() dto: UpdateAssignmentDto,
+    @Headers('assignment-token') token?: string,
+    @AuthUser() user?: UserToken,
+  ) {
+    await this.checkAuth(id, token, user);
     return await this.assignmentService.update(id, dto) ?? notFound(id);
   }
 
   @Delete(':id')
+  @Auth({optional: true})
   @ApiOkResponse({type: Assignment})
   @ApiNotFoundResponse()
-  async remove(@Param('id') id: string) {
+  @ApiForbiddenResponse({description: forbiddenResponse})
+  @ApiHeader({name: 'assignment-token', required: false})
+  async remove(
+    @Param('id') id: string,
+    @Headers('assignment-token') token?: string,
+    @AuthUser() user?: UserToken,
+  ) {
+    await this.checkAuth(id, token, user);
     return await this.assignmentService.remove(id) ?? notFound(id);
+  }
+
+  private isAuthorized(assignment: Assignment, headerToken: string, bearerToken: UserToken) {
+    return assignment.token === headerToken || bearerToken && bearerToken.sub === assignment.userId;
+  }
+
+  private async checkAuth(id: string, token: string, user: UserToken) {
+    const assignment = await this.assignmentService.findOne(id);
+    if (!assignment) {
+      notFound(id);
+    }
+    if (!this.isAuthorized(assignment, token, user)) {
+      throw new ForbiddenException(forbiddenResponse);
+    }
   }
 }
