b9a96f445c2362078705f55676d15c751dc48fcb
Adrian Kunz <clashsoft@hotmail.com>
2021-09-01 14:48:04 +0200

    refactor(assignments-service): Move generateToken to utils

--- services/apps/assignments/src/utils.ts
+++ services/apps/assignments/src/utils.ts
@@ -1,5 +1,13 @@
 import {NotFoundException} from '@nestjs/common';
+import {randomBytes} from 'crypto';
 
 export function notFound(msg: string): never {
   throw new NotFoundException(msg);
+}
+
+export function generateToken(): string {
+  const bytes = randomBytes(8);
+  const hex = bytes.toString('hex');
+  const [, a, b, c, d] = /(.{4})(.{4})(.{4})(.{4})/.exec(hex);
+  return `${a}-${b}-${c}-${d}`;
 }
