78fa43aeaa7950d463e9cccc586f424db13198f0
Adrian Kunz <clashsoft@hotmail.com>
2021-09-01 13:34:28 +0200

    feat(services): Optional auth

--- services/libs/keycloak-auth/src/auth.decorator.ts
+++ services/libs/keycloak-auth/src/auth.decorator.ts
@@ -1,13 +1,14 @@
 import {UserToken} from '@app/keycloak-auth/auth.interface';
+import {JwtAuthGuard} from '@app/keycloak-auth/jwt-auth.guard';
 import {applyDecorators, createParamDecorator, ExecutionContext, UseGuards} from '@nestjs/common';
 import {AuthGuard} from '@nestjs/passport';
 import {ApiBearerAuth, ApiUnauthorizedResponse} from '@nestjs/swagger';
 
 export const DEFAULT_DESCRIPTION = 'Missing or invalid Bearer token.';
 
-export function Auth() {
+export function Auth(options: {optional?: boolean}) {
   return applyDecorators(
-    UseGuards(AuthGuard('jwt')),
+    UseGuards(options.optional ? JwtAuthGuard : AuthGuard('jwt')),
     ApiBearerAuth(),
     ApiUnauthorizedResponse({
       description: DEFAULT_DESCRIPTION,
--- services/libs/keycloak-auth/src/jwt-auth.guard.ts
+++ services/libs/keycloak-auth/src/jwt-auth.guard.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import {Injectable} from '@nestjs/common';
+import {AuthGuard} from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest(err, user) {
+    return user;
+  }
 }
