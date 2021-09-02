ac64dbbf6d41b414d883332774d8c71b9ec41cd0
Adrian Kunz <clashsoft@hotmail.com>
2021-09-02 16:18:42 +0200

    fix(assignments-service): Sort solutions by timestamp

--- services/apps/assignments/src/solution/solution.service.ts
+++ services/apps/assignments/src/solution/solution.service.ts
@@ -27,7 +27,7 @@ export class SolutionService {
   }
 
   async findAll(where?: FilterQuery<Solution>): Promise<ReadSolutionDto[]> {
-    return this.model.find(where).select(['-token']).sort('+name').exec();
+    return this.model.find(where).select(['-token']).sort(['+name', '+timestamp']).exec();
   }
 
   async findOne(id: string): Promise<SolutionDocument | undefined> {
