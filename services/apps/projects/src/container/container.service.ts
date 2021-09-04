7128cc899b9d0bee587c74548f86bc82b25fdc36
Adrian Kunz <clashsoft@hotmail.com>
2021-09-04 21:17:15 +0200

    feat(projects-service): Implement create / start container

--- services/apps/projects/src/container/container.service.ts
+++ services/apps/projects/src/container/container.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@nestjs/common';
 import Dockerode from 'dockerode';
+import path from 'path';
 import {Container} from '../../../../../frontend/src/app/projects/model/container';
 import {environment} from '../environment';
 
@@ -12,27 +13,57 @@ export class ContainerService {
     version: environment.docker.version,
   });
 
-  async create(id: string): Promise<Container> {
-    throw new Error('Not implemented');
+  async create(projectId: string): Promise<Container> {
+    return await this.findOne(projectId) ?? await this.start(projectId);
   }
 
-  async findOne(id: string): Promise<Container | null> {
+  async start(projectId: string): Promise<Container> {
+    const bindPrefix = path.resolve(environment.docker.bindPrefix);
+    const container = await this.docker.createContainer({
+      Image: environment.docker.containerImage,
+      Tty: true,
+      NetworkingConfig: {
+        EndpointsConfig: {
+          'fulib-projects': {},
+        },
+      },
+      HostConfig: {
+        AutoRemove: true,
+        Binds: [
+          `${bindPrefix}:/projects/${projectId}`,
+        ],
+      },
+      Env: [
+        `PROJECT_ID=${projectId}`,
+      ],
+      Labels: {
+        'org.fulib.project': projectId,
+      },
+    });
+    await container.start();
+    return this.toContainer(container.id, projectId);
+  }
+
+  async findOne(projectId: string): Promise<Container | null> {
     // TODO can't we apply filters before listing ALL containers?
     const containers = await this.docker.listContainers({});
-    const container = containers.find(c => (c.Status === 'created' || c.Status === 'running') && c.Labels['org.fulib.project'] === id);
+    const container = containers.find(c => (c.Status === 'created' || c.Status === 'running') && c.Labels['org.fulib.project'] === projectId);
     if (!container) {
       return null;
     }
-
-    return {
-      id: container.Id,
-      projectId: id,
-      url: this.containerUrl(container.Id),
-    };
+    return this.toContainer(container.Id, projectId);
   }
 
   async remove(id: string): Promise<Container | null> {
     throw new Error('Not implemented');
+  }
+
+  private toContainer(id: string, projectId: string) {
+    return {
+      id,
+      projectId,
+      url: this.containerUrl(id),
+    };
   }
 
   private containerUrl(id: string): string {
