29819e8799198919aeca1b6a6ea9b2b552256517
Adrian Kunz <clashsoft@hotmail.com>
2021-09-04 19:51:47 +0200

    feat(projects-service): Implement main.ts and add environment.ts

---
+++ services/apps/projects/src/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  version: 'v1',
+  port: +(process.env.PORT || 6266),
+  mongo: {
+    uri: process.env.MONGO_URI || 'mongodb://fulibDotOrg:fulibDotOrg@localhost:27017/fulib-org',
+    options: {
+    },
+  },
+  auth: {
+    publicKey: `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjSudUTRH8wtEeIQdKh4Yv97HcFGjLzl9+yBm+SO2lf2r/jvpbSRPWqMDbrgQ0BGDwMQLweUoPUzOhZbrScs5edhRxtn3nPKUTiBtOoFiYupTTicdIKY1FAlagSM4/7WBbj0CT14cXQoyZK2VbfYXsu19tdJ/oFPBAGwrsj/ugiGE6c8H6wu4yHPsJFDR8sN524NN4diXOKyZ731IP5lqfN4+bP17/b1KaDG8swsRNW93dfEz0nfcJu+wutI3dLc98/cekOpHUZRzndsshifjuM39RF6oyI89tcuwrrKqrz5HV9CsPBUB2hakGBhg71svZyq304ikQFYej/ydZd0biwIDAQAB
+-----END PUBLIC KEY-----`,
+    resource: process.env.AUTH_RESOURCE || 'projects-service',
+    algorithms: (process.env.AUTH_ALGORITHMS || 'RS256').split(','),
+    issuer: process.env.AUTH_ISSUER || 'https://se.uniks.de/auth/realms/fulib.org',
+  },
+};
