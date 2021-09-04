986d09423ace0bf6dbf23bb85f5d4c70407d408b
Adrian Kunz <clashsoft@hotmail.com>
2021-09-04 19:52:15 +0200

    feat(projects-service): Add basic module imports

--- services/apps/projects/src/projects.module.ts
+++ services/apps/projects/src/projects.module.ts
@@ -1,7 +1,13 @@
+import {AuthModule} from '@app/keycloak-auth';
 import {Module} from '@nestjs/common';
+import {MongooseModule} from '@nestjs/mongoose';
+import {environment} from '../../assignments/src/environment';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(environment.mongo.uri, environment.mongo.options),
+    AuthModule.register(environment.auth),
+  ],
   controllers: [],
   providers: [],
 })
