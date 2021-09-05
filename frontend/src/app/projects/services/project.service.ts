0501f4a31367b5eea45dabb582c2ad42495035ce
Adrian Kunz <clashsoft@hotmail.com>
2021-09-05 15:56:11 +0200

    refactor(frontend): Simplify ProjectService.getOwn

--- frontend/src/app/projects/services/project.service.ts
+++ frontend/src/app/projects/services/project.service.ts
@@ -78,14 +78,7 @@ export class ProjectService {
 
   getOwn(): Observable<Project[]> {
     return this.users.current$.pipe(
-      switchMap(user => {
-        if (!user || !user.id) {
-          return of([]);
-        }
-        return this.http.get<Project[]>(`${environment.projectsApiUrl}/projects`, {
-          params: {userId: user.id},
-        });
-      }),
+      switchMap(user => user ? this.http.get<Project[]>(`${environment.projectsApiUrl}/projects`) : of([])),
       map(projects => [...this.localProjectService.getAll(), ...projects]),
     );
   }
