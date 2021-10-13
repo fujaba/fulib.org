1b609e4f84e161180bb23ca55253e374ae404035
Adrian Kunz <clashsoft@hotmail.com>
2021-10-14 01:18:13 +0200

    fix(frontend): Use correct Projects API URL in prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiURL: '/api',
   assignmentsApiUrl: '/api/v1',
-  projectsApiUrl: '/api',
+  projectsApiUrl: '/api/v1',
   auth: {
     url: 'https://se.uniks.de/auth',
     realm: 'fulib.org',
