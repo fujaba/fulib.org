283e2386793ede060e90f13fda49e39f8e3dcca6
Adrian Kunz <clashsoft@hotmail.com>
2021-10-21 14:35:22 +0200

    fix(assignments-service): Properly calculate negative point totals

--- services/apps/assignments/src/assignment/assignment.service.ts
+++ services/apps/assignments/src/assignment/assignment.service.ts
@@ -65,6 +65,37 @@ export class AssignmentService {
     };
   }
 
+  createPointsCache(tasks: Task[], evaluations: Record<string, CreateEvaluationDto>): Record<string, number> {
+    const cache = {};
+    for (let task of tasks) {
+      this.getTaskPoints(task, evaluations, cache);
+    }
+    return cache;
+  }
+
+  private getTaskPoints(task: Task, evaluations: Record<string, CreateEvaluationDto>, cache: Record<string, number>): number {
+    return cache[task._id] ??= this.calculateTaskPoints(task, evaluations, cache);
+  }
+
+  private calculateTaskPoints(task: Task, evaluations: Record<string, CreateEvaluationDto>, cache: Record<string, number>): number {
+    const evaluation = evaluations?.[task._id];
+    if (evaluation) {
+      // An evaluation overrides children.
+      return evaluation.points;
+    }
+
+    if (!task.children.length) {
+      // A task with positive points but no children defaults to being failed.
+      return 0;
+    }
+
+    const positiveChildDeduction = task.children.reduce((a, c) => c.points > 0 ? a + c.points : a, 0);
+    // A task with children is granted, by default, its total points minus the total of positive children
+    const basePoints = task.points - positiveChildDeduction;
+    const childSum = task.children.reduce((a, c) => a + this.getTaskPoints(c, evaluations, cache), 0);
+    return basePoints + childSum;
+  }
+
   async create(dto: CreateAssignmentDto, userId?: string): Promise<AssignmentDocument> {
     const token = generateToken();
     return this.model.create({
