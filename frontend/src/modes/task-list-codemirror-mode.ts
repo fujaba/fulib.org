8c2c53e9e1a9650fe157ad7e885579af2594f865
Adrian Kunz <clashsoft@hotmail.com>
2021-11-15 13:29:36 +0100

    fix(frontend): Ignore h1-level (#) tasks in Markdown

--- frontend/src/modes/task-list-codemirror-mode.ts
+++ frontend/src/modes/task-list-codemirror-mode.ts
@@ -1,6 +1,6 @@
 import {Rule} from 'codemirror';
 
-export const TASK_ITEM_PATTERN = /(#+|-)(\s+)(.*)(\s+\((?:[x\d]+P?\/)?)(-?\d+)(P?\))(\s*<!--[a-zA-Z0-9]+-->)?/;
+export const TASK_ITEM_PATTERN = /(#{2,}|-)(\s+)(.*)(\s+\((?:[x\d]+P?\/)?)(-?\d+)(P?\))(\s*<!--[a-zA-Z0-9]+-->)?/;
 
 export function extractTaskItem([_line, prefix, _1, description, _3, points, _4, id]: string[]) {
   id = id?.trim()?.slice(4, -3);
