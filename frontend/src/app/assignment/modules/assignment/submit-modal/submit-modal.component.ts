862d681f1d51f4b187ddbdc5ee1edabdc4a41e4e
Adrian Kunz <clashsoft@hotmail.com>
2021-11-18 12:01:00 +0100

    feat(frontend): Implement Submit as Email, Issue Draft and Issue

--- frontend/src/app/assignment/modules/assignment/submit-modal/submit-modal.component.ts
+++ frontend/src/app/assignment/modules/assignment/submit-modal/submit-modal.component.ts
@@ -2,10 +2,12 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {forkJoin} from 'rxjs';
 import {switchMap} from 'rxjs/operators';
+import {ToastService} from '../../../../toast.service';
 import Assignment from '../../../model/assignment';
 import Solution from '../../../model/solution';
 import {AssignmentService} from '../../../services/assignment.service';
 import {SolutionService} from '../../../services/solution.service';
+import {IssueDto, SubmitService} from '../submit.service';
 
 @Component({
   selector: 'app-submit-modal',
@@ -15,12 +17,18 @@ import {SolutionService} from '../../../services/solution.service';
 export class SubmitModalComponent implements OnInit {
   assignment?: Assignment;
   solution?: Solution;
+  issue?: IssueDto;
+
+  mailLink?: string;
+  githubToken?: string;
   draftLink?: string;
 
   constructor(
     public route: ActivatedRoute,
     private assignmentService: AssignmentService,
     private solutionService: SolutionService,
+    private submitService: SubmitService,
+    private toastService: ToastService,
   ) {
   }
 
@@ -29,22 +37,38 @@ export class SubmitModalComponent implements OnInit {
       switchMap(({aid, sid}) => forkJoin([
         this.assignmentService.get(aid),
         this.solutionService.get(aid, sid),
-        this.solutionService.getEvaluations(aid, sid),
       ])),
-    ).subscribe(([assignment, solution, evaluations]) => {
+    ).subscribe(([assignment, solution]) => {
       this.assignment = assignment;
       this.solution = solution;
-
-      const org = assignment.classroom?.org;
-      const prefix = assignment.classroom?.prefix;
-      const githubUsername = solution.author?.github;
-      const title = assignment.title;
-      const body = 'test';
-      this.draftLink = `https://github.com/${org}/${prefix}-${githubUsername}/issues/new?title=${encodeURIComponent(title)}&body=${encodeURIComponent(body)}`;
+      this.submitService.getGitHubToken().then(token => this.githubToken = token);
+      this.submitService.createIssue(assignment, solution).then(issue => this.setIssue(issue));
     });
   }
 
-  create() {
+  private setIssue(issue: IssueDto) {
+    this.issue = issue;
+    const {title, body} = issue;
+    const encodedTitle = encodeURIComponent(title);
+    const encodedBody = encodeURIComponent(body);
 
+    const {org, prefix} = this.assignment?.classroom ?? {};
+    const {email, github} = this.solution?.author ?? {};
+    if (org && prefix && github) {
+      this.draftLink = `https://github.com/${org}/${prefix}-${github}/issues/new?title=${encodedTitle}&body=${encodedBody}`;
+    }
+
+    if (email) {
+      this.mailLink = `mailto:${email}?subject=${encodedTitle}&body=${encodedBody}`;
+    }
+  }
+
+  async submitIssue() {
+    try {
+      const issue = await this.submitService.postIssueToGitHub(this.assignment!, this.solution!, this.issue!, this.githubToken!);
+      this.toastService.success('Submit', `Successfully posted issue #${issue.number}`);
+    } catch (error) {
+      this.toastService.error('Submit', 'Failed to post issue', error);
+    }
   }
 }
