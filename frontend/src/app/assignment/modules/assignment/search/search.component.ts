0d0e84707f15fd4a26463ebe4ecd7e1600a97a77
Adrian Kunz <clashsoft@hotmail.com>
2021-11-24 21:08:57 +0100

    fix(frontend): Correctly show solutions in search

--- frontend/src/app/assignment/modules/assignment/search/search.component.ts
+++ frontend/src/app/assignment/modules/assignment/search/search.component.ts
@@ -1,10 +1,12 @@
 import {AfterViewChecked, Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import hljs from 'highlight.js/lib/core';
-import {BehaviorSubject, combineLatest} from 'rxjs';
-import {debounceTime, distinctUntilChanged, switchMap} from 'rxjs/operators';
+import {BehaviorSubject, combineLatest, forkJoin} from 'rxjs';
+import {debounceTime, distinctUntilChanged, switchMap, tap} from 'rxjs/operators';
 import {SearchResult} from '../../../model/search-result';
+import Solution from '../../../model/solution';
 import {AssignmentService} from '../../../services/assignment.service';
+import {SolutionService} from '../../../services/solution.service';
 
 @Component({
   selector: 'app-assignment-search',
@@ -15,8 +17,10 @@ export class SearchComponent implements OnInit {
   search$ = new BehaviorSubject<string>(this.route.snapshot.queryParams.q);
 
   results: SearchResult[] = [];
+  solutions: Record<string, Solution> = {};
 
   constructor(
+    private solutionService: SolutionService,
     private assignmentService: AssignmentService,
     private route: ActivatedRoute,
     private router: Router,
@@ -24,6 +28,15 @@ export class SearchComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.route.params.pipe(
+      switchMap(({aid}) => this.solutionService.getAll(aid)),
+    ).subscribe(solutions => {
+      this.solutions = {};
+      for (let solution of solutions) {
+        this.solutions[solution._id!] = solution;
+      }
+    });
+
     combineLatest([
       this.route.params,
       this.route.queryParams,
