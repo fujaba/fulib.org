29a1e118f5dfc666ee6fea582d212fb67e46bbd0
Adrian Kunz <clashsoft@hotmail.com>
2021-11-24 18:39:28 +0100

    feat(frontend): Show search snippets context

--- frontend/src/app/assignment/model/search-result.ts
+++ frontend/src/app/assignment/model/search-result.ts
@@ -3,5 +3,5 @@ import {Snippet} from './evaluation';
 export interface SearchResult {
   assignment: string;
   solution: string;
-  snippets: Snippet[];
+  snippets: (Snippet & { context: string })[];
 }
