8b3a43c17233bfa3f3b858cf2d2b6e0c74b9a293
Adrian Kunz <clashsoft@hotmail.com>
2021-11-25 21:36:14 +0100

    fix(assignments-service): Re-run code search when updating evaluation

--- services/apps/assignments/src/evaluation/evaluation.service.ts
+++ services/apps/assignments/src/evaluation/evaluation.service.ts
@@ -16,7 +16,7 @@ export class EvaluationService {
   async create(assignment: string, solution: string, dto: CreateEvaluationDto, createdBy?: string): Promise<Evaluation> {
     if (dto.codeSearch && dto.snippets.length) {
       delete dto.codeSearch;
-      this.codeSearch(assignment, dto);
+      this.codeSearchCreate(assignment, dto);
     }
 
     return this.model.create({
@@ -27,14 +27,40 @@ export class EvaluationService {
     });
   }
 
-  private async codeSearch(assignment: string, dto: CreateEvaluationDto) {
-    const resultss = await Promise.all(dto.snippets.map(snippet => this.searchService.find(assignment, snippet.code)));
+  async findAll(where: FilterQuery<Evaluation> = {}): Promise<Evaluation[]> {
+    return this.model.find(where).exec();
+  }
+
+  async findOne(id: string): Promise<Evaluation | null> {
+    return this.model.findById(id).exec();
+  }
+
+  async update(id: string, dto: UpdateEvaluationDto): Promise<Evaluation | null> {
+    const evaluation = await this.model.findByIdAndUpdate(id, dto, {new: true}).exec();
+    if (evaluation && dto.codeSearch && dto.snippets && dto.snippets.length) {
+      delete dto.codeSearch;
+      this.codeSearchUpdate(evaluation.assignment, dto);
+    }
+    return evaluation;
+  }
+
+  async remove(id: string): Promise<Evaluation | null> {
+    return this.model.findByIdAndDelete(id).exec();
+  }
+
+  private async codeSearch(assignment: string, snippets: Snippet[]): Promise<Record<string, Snippet[]>> {
+    const resultss = await Promise.all(snippets.map(snippet => this.searchService.find(assignment, snippet.code)));
     const solutions: Record<string, Snippet[]> = {};
     for (let results of resultss) {
       for (let result of results) {
         (solutions[result.solution] ??= []).push(...result.snippets);
       }
     }
+    return solutions;
+  }
+
+  private async codeSearchCreate(assignment: string, dto: CreateEvaluationDto) {
+    const solutions = await this.codeSearch(assignment, dto.snippets);
     return this.model.bulkWrite(Object.entries(solutions).map(([solution, snippets]) => {
       const filter: FilterQuery<Evaluation> = {
         assignment,
@@ -56,19 +82,25 @@ export class EvaluationService {
     }));
   }
 
-  async findAll(where: FilterQuery<Evaluation> = {}): Promise<Evaluation[]> {
-    return this.model.find(where).exec();
-  }
-
-  async findOne(id: string): Promise<Evaluation | null> {
-    return this.model.findById(id).exec();
-  }
-
-  async update(id: string, dto: UpdateEvaluationDto): Promise<Evaluation | null> {
-    return this.model.findByIdAndUpdate(id, dto, {new: true}).exec();
-  }
-
-  async remove(id: string): Promise<Evaluation | null> {
-    return this.model.findByIdAndDelete(id).exec();
+  private async codeSearchUpdate(assignment: string, dto: UpdateEvaluationDto) {
+    const solutions = await this.codeSearch(assignment, dto.snippets!);
+    return this.model.bulkWrite(Object.entries(solutions).map(([solution, snippets]) => {
+      const filter: FilterQuery<Evaluation> = {
+        assignment,
+        solution,
+        task: dto.task,
+        author: 'Code Search',
+      };
+      const updatedEvaluation: UpdateEvaluationDto = {
+        ...dto,
+        snippets,
+      };
+      return {
+        updateOne: {
+          filter,
+          update: {$set: updatedEvaluation},
+        },
+      };
+    }));
   }
 }
