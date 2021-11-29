3af41cb6b0d659515480b4c41d489ad7d9fa03a8
Adrian Kunz <clashsoft@hotmail.com>
2021-11-29 01:07:55 +0100

    refactor(frontend): Use backend to fetch statistics

--- frontend/src/app/assignment/modules/assignment/statistics/statistics.component.ts
+++ frontend/src/app/assignment/modules/assignment/statistics/statistics.component.ts
@@ -2,16 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {forkJoin} from 'rxjs';
 import {switchMap} from 'rxjs/operators';
-import Task from '../../../model/task';
 import {AssignmentService} from '../../../services/assignment.service';
-import {SolutionService} from '../../../services/solution.service';
 import {TaskService} from '../../../services/task.service';
+import {AssignmentStatistics, StatisticsService} from '../statistics.service';
 
-interface StatisticItem {
-  tasks: Task[];
-  totalPoints: number;
-  totalGiven: number;
-}
 
 @Component({
   selector: 'app-assignment-statistics',
@@ -19,15 +13,11 @@ interface StatisticItem {
   styleUrls: ['./statistics.component.scss'],
 })
 export class StatisticsComponent implements OnInit {
-  results: StatisticItem[] = [];
-  totalSolutions = 0;
-  totalEvaluations = 0;
-  codeSearchEvaluations = 0;
-  codeSearchSolutions = 0;
+  stats?: AssignmentStatistics;
 
   constructor(
     private assignmentService: AssignmentService,
-    private solutionService: SolutionService,
+    private statisticsService: StatisticsService,
     private taskService: TaskService,
     private route: ActivatedRoute,
   ) {
@@ -37,44 +27,13 @@ export class StatisticsComponent implements OnInit {
     this.route.params.pipe(
       switchMap(({aid}) => forkJoin([
         this.assignmentService.get(aid),
-        this.solutionService.getEvaluations(aid),
+        this.statisticsService.getAssignmentStatistics(aid),
       ])),
-    ).subscribe(([assignment, evaluations]) => {
-      const solutions = new Set<string>();
-      const codeSearchSolutions = new Set<string>();
-      const taskTotals = new Map<string, StatisticItem>();
-      let codeSearchEvaluations = 0;
-      for (let evaluation of evaluations) {
-        solutions.add(evaluation.solution);
-
-        if (evaluation.codeSearch?.origin) {
-          codeSearchEvaluations++;
-          codeSearchSolutions.add(evaluation.solution);
-        }
-
-        const task = evaluation.task;
-
-        let item = taskTotals.get(task);
-        if (!item) {
-          item = {
-            totalPoints: 0,
-            totalGiven: 0,
-            tasks: this.taskService.findWithParents(assignment.tasks, task),
-          };
-          taskTotals.set(task, item);
-        }
-
-        item.totalPoints += evaluation.points;
-        item.totalGiven++;
+    ).subscribe(([assignment, statistics]) => {
+      this.stats = statistics;
+      for (let task of statistics.tasks) {
+        task.resolved = this.taskService.findWithParents(assignment.tasks, task.task);
       }
-
-      this.totalSolutions = solutions.size;
-      this.totalEvaluations = evaluations.length;
-      this.codeSearchEvaluations = codeSearchEvaluations;
-      this.codeSearchSolutions = codeSearchSolutions.size;
-      this.results = Array.from(taskTotals.values())
-        .sort((a, b) => b.totalPoints - a.totalPoints)
-      ;
     });
   }
 }
