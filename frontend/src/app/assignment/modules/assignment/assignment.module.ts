81f58cb0feccf53adb0669edd7eda20faaab375d
Adrian Kunz <clashsoft@hotmail.com>
2021-12-23 16:50:12 +0100

    refactor(frontend): Add assignments config service

--- frontend/src/app/assignment/modules/assignment/assignment.module.ts
+++ frontend/src/app/assignment/modules/assignment/assignment.module.ts
@@ -8,13 +8,14 @@ import {AssignmentSharedModule} from '../shared/shared.module';
 
 import {AssignmentRoutingModule} from './assignment-routing.module';
 import {AssignmentComponent} from './assignment/assignment.component';
-import {SubmitModalComponent} from './submit-modal/submit-modal.component';
+import {ConfigService} from './config.service';
+import {SearchComponent} from './search/search.component';
 import {ShareComponent} from './share/share.component';
 import {SolutionTableComponent} from './solution-table/solution-table.component';
+import {StatisticValueComponent} from './statistic-value/statistic-value.component';
 import {StatisticsComponent} from './statistics/statistics.component';
+import {SubmitModalComponent} from './submit-modal/submit-modal.component';
 import {AssignmentTasksComponent} from './tasks/tasks.component';
-import { SearchComponent } from './search/search.component';
-import { StatisticValueComponent } from './statistic-value/statistic-value.component';
 
 
 @NgModule({
@@ -28,18 +29,21 @@ import { StatisticValueComponent } from './statistic-value/statistic-value.compo
     SearchComponent,
     StatisticValueComponent,
   ],
-    imports: [
-        CommonModule,
-        SharedModule,
-        FormsModule,
-        ClipboardModule,
-        NgbTypeaheadModule,
-        NgbPopoverModule,
-        NgbTooltipModule,
-        AssignmentSharedModule,
-        AssignmentRoutingModule,
-        NgbDropdownModule,
-    ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    FormsModule,
+    ClipboardModule,
+    NgbTypeaheadModule,
+    NgbPopoverModule,
+    NgbTooltipModule,
+    AssignmentSharedModule,
+    AssignmentRoutingModule,
+    NgbDropdownModule,
+  ],
+  providers: [
+    ConfigService,
+  ],
 })
 export class AssignmentModule {
 }
---
+++ frontend/src/app/assignment/modules/assignment/config.service.ts
@@ -0,0 +1,52 @@
+import {Injectable} from '@angular/core';
+import {PrivacyService} from '../../../privacy.service';
+
+export type ConfigKey =
+  | 'ide'
+  | 'cloneProtocol'
+  ;
+
+export interface ConfigOption {
+  key: ConfigKey;
+  title: string;
+  options: [string, string][];
+  default: string;
+}
+
+export const CONFIG_OPTIONS: ConfigOption[] = [
+  {
+    key: 'ide',
+    title: 'IDE',
+    options: [['vscode', 'VSCode'], ['code-oss', 'Code - OSS'], ['vscodium', 'VSCodium']],
+    default: 'vscode',
+  },
+  {
+    key: 'cloneProtocol',
+    title: 'Git Clone Protocol',
+    options: [['https', 'HTTPS'], ['ssh', 'SSH']],
+    default: 'https',
+  },
+];
+
+@Injectable()
+export class ConfigService {
+  constructor(
+    private privacyService: PrivacyService,
+  ) {
+  }
+
+  get(key: ConfigKey): string {
+    const option = CONFIG_OPTIONS.find(o => o.key === key);
+    return this.privacyService.getStorage('assignments/' + key) || option?.default || '';
+  }
+
+  set(key: ConfigKey, value: string) {
+    if (this.getOption(key)) {
+      this.privacyService.setStorage('assignments/' + key, value);
+    }
+  }
+
+  private getOption(key: ConfigKey): ConfigOption | undefined {
+    return CONFIG_OPTIONS.find(o => o.key === key);
+  }
+}
