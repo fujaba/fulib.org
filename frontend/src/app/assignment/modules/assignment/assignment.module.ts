98139dbb3f3b7aad985b0f8bfead90b85caefd70
Adrian Kunz <clashsoft@hotmail.com>
2022-01-02 23:38:03 +0100

    feat(frontend): Re-add GitHub solution import

--- frontend/src/app/assignment/modules/assignment/assignment.module.ts
+++ frontend/src/app/assignment/modules/assignment/assignment.module.ts
@@ -1,13 +1,20 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {NgbDropdownModule, NgbPopoverModule, NgbTooltipModule, NgbTypeaheadModule} from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbDropdownModule,
+  NgbNavModule,
+  NgbPopoverModule,
+  NgbTooltipModule,
+  NgbTypeaheadModule,
+} from '@ng-bootstrap/ng-bootstrap';
 import {ClipboardModule} from 'ngx-clipboard';
 import {SharedModule} from '../../../shared/shared.module';
 import {AssignmentSharedModule} from '../shared/shared.module';
 
 import {AssignmentRoutingModule} from './assignment-routing.module';
 import {AssignmentComponent} from './assignment/assignment.component';
+import {ImportModalComponent} from './import-modal/import-modal.component';
 import {SearchComponent} from './search/search.component';
 import {ShareComponent} from './share/share.component';
 import {SolutionTableComponent} from './solution-table/solution-table.component';
@@ -15,7 +22,6 @@ import {StatisticValueComponent} from './statistic-value/statistic-value.compone
 import {StatisticsComponent} from './statistics/statistics.component';
 import {SubmitModalComponent} from './submit-modal/submit-modal.component';
 import {AssignmentTasksComponent} from './tasks/tasks.component';
-import { ImportModalComponent } from './import-modal/import-modal.component';
 
 
 @NgModule({
@@ -41,6 +47,7 @@ import { ImportModalComponent } from './import-modal/import-modal.component';
     AssignmentSharedModule,
     AssignmentRoutingModule,
     NgbDropdownModule,
+    NgbNavModule,
   ],
 })
 export class AssignmentModule {
