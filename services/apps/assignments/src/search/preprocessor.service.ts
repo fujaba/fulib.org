4025529cacd75d54df7d2ca7d60618ae5ce1546a
Adrian Kunz <clashsoft@hotmail.com>
2022-01-11 22:47:57 +0100

    feat(assignments-service): Start renaming Java variables

--- services/apps/assignments/src/search/preprocessor.service.ts
+++ services/apps/assignments/src/search/preprocessor.service.ts
@@ -1,5 +1,9 @@
 import {Injectable} from '@nestjs/common';
 
+const JAVA_KEYWORDS = new Set(`abstract continue for new switch assert default goto package synchronized boolean do if private this \
+break implements protected throw else import public throws case enum instanceof return transient catch extends try \
+final interface static void class finally strictfp volatile const native super while`.split(' '));
+
 @Injectable()
 export class PreprocessorService {
   preprocess(code: string): string {
@@ -10,11 +14,30 @@ export class PreprocessorService {
 
   stripBlockComments(code: string): string {
     return code.replace(/\/\*.*?\*\//gs, comment => {
-      return comment.replace(/[^\t\r\n]/g, ' ');
+      return comment.replace(/[^ \r\n]/g, ' ');
     });
   }
 
   stripLineComments(code: string): string {
     return code.replace(/(\/\/|#).*\n/g, '\n');
+  }
+
+  renameJavaVariables(code: string): string {
+    const remapping = new Map<string, string>();
+    const typeIndex = new Map<string, number>();
+    code = code.replace(/([a-zA-Z0-9$_<>\[\]]+)(\s+)([a-zA-Z0-9$_]{2,})/g, (decl, type, space, name) => {
+      if (JAVA_KEYWORDS.has(type)) {
+        return decl;
+      }
+      const shortType = type.charAt(0).toLowerCase();
+      const index = typeIndex.get(shortType) ?? 0;
+      typeIndex.set(shortType, index + 1);
+      const indexStr = index.toString(36);
+      const newName = shortType + indexStr;
+      remapping.set(name, newName);
+      return type + space + newName.padEnd(name.length, ' ');
+    });
+    code = code.replace(/([a-zA-Z0-9$_]{2,})/g, name => remapping.get(name)?.padEnd(name.length, ' ') ?? name);
+    return code;
   }
 }
