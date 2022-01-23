eb40084596f694a5bd556f0af2a094c82b6e7e49
Adrian Kunz <clashsoft@hotmail.com>
2022-01-23 22:55:41 +0100

    feat(frontend): Duration pipe

---
+++ frontend/src/app/shared/duration.pipe.ts
@@ -0,0 +1,19 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+  name: 'duration',
+})
+export class DurationPipe implements PipeTransform {
+
+  transform(value: number): unknown {
+    value |= 0;
+    const seconds = value % 60;
+    const minutes = 0 | (value / 60) % 60;
+    const hours = 0 | (value / 3600);
+    return [
+      hours && hours + 'h',
+      minutes && minutes + 'm',
+      seconds && seconds + 's',
+    ].filter(x => x).join(' ');
+  }
+}
