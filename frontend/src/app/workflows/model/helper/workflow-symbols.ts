cfc498aee12345d50e7c1e70edc36113bede26e8
Maximilian Freiherr von Künßberg <maximilian-kuenssberg@t-online.de>
2022-02-28 14:47:43 +0100

    fix(workflows): implement fixes for workflow hinter

---
+++ frontend/src/app/workflows/model/helper/workflow-symbols.ts
@@ -0,0 +1,28 @@
+// keywords for fulibWorkflows v0.4.1
+export const noteKeywords = [
+  'workflow',
+  'externalSystem',
+  'service',
+  'command',
+  'event',
+  'policy',
+  'user',
+  'data',
+  'page',
+  'problem',
+];
+
+export const pageKeywords = [
+  'pageName',
+  'text',
+  'input',
+  'password',
+  'button',
+  'value',
+  'targetPage',
+];
+
+export const allKeywords = [
+  ...noteKeywords,
+  ...pageKeywords,
+];
--- frontend/src/app/workflows/model/helper/workflows-hint.ts
+++ frontend/src/app/workflows/model/helper/workflows-hint.ts
@@ -1,29 +1,17 @@
 import * as CodeMirror from 'codemirror';
+import {allKeywords} from './workflow-symbols';
 
 export function cmWorkflowsHint(cm) {
-  const noteKeywords = ['workflow', 'externalSystem', 'service', 'command', 'event', 'policy', 'user', 'data', 'page', 'problem'];
-  const pageKeywords = ['pageName', 'text', 'input', 'password', 'button', 'value', 'targetPage'];
   const cur = cm.getCursor();
   const range = cm.findWordAt(cur);
-  let start = range.anchor.ch;
-  let end = range.head.ch;
+  const start = range.anchor.ch;
+  const end = range.head.ch;
 
   // Get Current Word
   const word = cm.getRange(range.anchor, range.head);
 
   // Filter Keywords for possible completions for the current word
-  const result: string[] = [];
-  for (const noteKeyword of noteKeywords) {
-    if (!word || word === ' ' || noteKeyword.indexOf(word) === 0) {
-      result.push(noteKeyword);
-    }
-  }
-
-  for (const pageKeyword of pageKeywords) {
-    if (!word || word === ' ' || pageKeyword.indexOf(word) === 0) {
-      result.push(pageKeyword);
-    }
-  }
+  const result = word.trim() ? allKeywords.filter(keyword => keyword.startsWith(word)) : allKeywords;
 
   return {
     list: result,
