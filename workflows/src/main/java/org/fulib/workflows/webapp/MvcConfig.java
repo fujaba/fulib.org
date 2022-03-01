320997626541e5eae9dd4fc8d1331fbcabc55c59
Maximilian Freiherr von Künßberg <maximilian-kuenssberg@t-online.de>
2022-03-01 18:24:19 +0100

    fix(workflows): fix os problem for all developers hopefully

--- workflows/src/main/java/org/fulib/workflows/webapp/MvcConfig.java
+++ workflows/src/main/java/org/fulib/workflows/webapp/MvcConfig.java
@@ -8,8 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/workflows/**")
-                .addResourceLocations("file://" + System.getProperty("java.io.tmpdir") + "/fulib.org/workflows/")
-                .setCachePeriod(60 * 60);
+        String osName = System.getProperty("os.name");
+        if (osName.contains("Windows") || osName.contains("windows")) {
+            registry.addResourceHandler("/workflows/**")
+                    .addResourceLocations("file:/" + System.getProperty("java.io.tmpdir") + "/fulib.org/workflows/")
+                    .setCachePeriod(60 * 60);
+        } else {
+            registry.addResourceHandler("/workflows/**")
+                    .addResourceLocations("file://" + System.getProperty("java.io.tmpdir") + "/fulib.org/workflows/")
+                    .setCachePeriod(60 * 60);
+        }
     }
 }
