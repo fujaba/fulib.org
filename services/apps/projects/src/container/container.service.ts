1a0f10db5ba3bc03a85d0381ca798ecaa286ca39
Alexej Fromm <alexej.fromm@gmx.de>
2022-06-20 12:36:33 +0200

    /healthz endpoint and extensions handling
    -heartbeatfile replaced with http /healthz endpoint,
    -bind settings.json file only
    -preinstall java extension in Dockerfile
    -install extensions with help of a list, extension no longer persisted due size concerns

--- services/apps/projects/src/container/container.service.ts
+++ services/apps/projects/src/container/container.service.ts
@@ -4,18 +4,17 @@ import * as Dockerode from 'dockerode';
 import * as path from 'path';
 import {environment} from '../environment';
 import {ContainerDto} from './container.dto';
-import * as fs from 'fs';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import { setTimeout } from "timers/promises";
-
-// when there is no connection for at least IDLE_TIME (ms) to a code-server container,
-// the container will stop
-// 300.000ms = 5min
-const IDLE_TIME = 300_000
+import { setTimeout } from 'timers/promises';
+import {HttpService} from '@nestjs/axios';
+import {firstValueFrom} from 'rxjs';
+import * as fs from 'fs';
 
 
 @Injectable()
 export class ContainerService {
+
+  constructor(private readonly httpService: HttpService) {}
 
   private codeWorkspace: string = '/home/coder/project';
 
@@ -39,6 +38,21 @@ export class ContainerService {
   async start(projectId: string): Promise<ContainerDto> {
     const bindPrefix = path.resolve(environment.docker.bindPrefix);
     const token = randomBytes(10).toString('base64');
+
+    /* create 'settings.json' file if it doesn't exist already
+    code server will write the user settings there
+    if we won't create the file, docker will automatically
+    create a directory instead, which will lead to an error
+    when code server is trying to write on the bind mount
+     */
+    const settingsFile = `${bindPrefix}/config/${this.idBin(projectId)}/${projectId}/User/settings.json`
+    const settingsDir = `${bindPrefix}/config/${this.idBin(projectId)}/${projectId}/User/`
+    await fs.promises.readFile(settingsFile).catch( async () => {
+      await fs.promises.mkdir(settingsDir, {recursive: true});
+      await fs.promises.writeFile(settingsFile, "");
+    });
+
+
     const container = await this.docker.createContainer({
       Image: environment.docker.containerImage,
       Tty: true,
@@ -51,14 +65,11 @@ export class ContainerService {
       HostConfig: {
         AutoRemove: true,
         Binds: [
+          //workspace bind
           `${bindPrefix}/projects/${this.idBin(projectId)}/${projectId}:${this.codeWorkspace}`,
 
-
-          // bind configuration files (for example extensions and user settings can be found in here)
-          // the heartbeat file is also included there (/home/coder/.local/share/code-server/heartbeat)
-          // heartbeatfile: is modified by code-server every minute if there's an active connection
-          `${bindPrefix}/config/${this.idBin(projectId)}/${projectId}:/home/coder/.local/share/code-server`,
-
+          // vs code settings bind
+          `${bindPrefix}/config/${this.idBin(projectId)}/${projectId}/User/settings.json:/home/coder/.local/share/code-server/User/settings.json`,
         ],
       },
       Env: [
@@ -74,7 +85,6 @@ export class ContainerService {
     });
 
     await container.start();
-    await this.preinstallExtension(container);
 
     // wait 1s for container startup
     // without waiting we're getting 502 - Bad Gateway Error on the code server iframe on first loading
@@ -82,6 +92,10 @@ export class ContainerService {
       await setTimeout(1000);
     };
     await wait();
+
+    // loop over extensions list and install all extensions
+    await this.installExtensions(container, projectId);
+
     return this.toContainer(container.id, token, projectId);
   }
 
@@ -106,9 +120,18 @@ export class ContainerService {
       return null;
     }
     const container = this.docker.getContainer(existing.id);
+
+    // get extensions and write in list, before stopping the container
+    const stream = await this.containerExec(container,['code-server', '--list-extensions']);
+    const bindPrefix = path.resolve(environment.docker.bindPrefix);
+    const extensionsList = `${bindPrefix}/config/${this.idBin(projectId)}/${projectId}/User/extensions.txt`;
+    let writeStream = fs.createWriteStream(extensionsList);
+    stream.pipe(writeStream);
+
     await container.stop();
     return existing;
   }
+
 
   private toContainer(id: string, projectId: string, token: string): ContainerDto {
     return {
@@ -140,34 +163,25 @@ export class ContainerService {
     for(let i = 0; i < containers.length; i++) {
       let container = containers[i];
       let projectId = container['Labels']['org.fulib.project'];
-      let p = this.getHeartbeatPath(projectId);
 
-      if(await this.isHeartbeatExpired(p)) {
+      if(await this.isHeartbeatExpired(container.Id)) {
         this.remove(projectId);
       }
     }
 
   }
 
-  private getHeartbeatPath(projectId: string) {
-    const bindPrefix = path.resolve(environment.docker.bindPrefix);
-    return path.resolve(`${bindPrefix}/config/${this.idBin(projectId)}/${projectId}/heartbeat`);
+  private async isHeartbeatExpired(containerId: string) : Promise<boolean> {
+    const url = `${environment.docker.proxyHost}/containers/${containerId.substring(0, 12)}`;
+    const res =  await firstValueFrom(this.httpService.get(`${url}/healthz`));
+    return (res.data.status === 'expired');
   }
 
-  private async isHeartbeatExpired(path: string) : Promise<boolean> {
-    // fetch file details
-    const stat = await fs.promises.stat(path);
-    let heartbeatTime = stat.mtime.getTime();
-    let currentTime = Date.now();
-    return currentTime - heartbeatTime >= IDLE_TIME;
-  }
-
-
-  //install a vs code extension by simply copy the extension folder in the related directory
-  private async preinstallExtension(container: Dockerode.Container) {
+  //executes command in container and returns the output stream
+  private async containerExec(container: Dockerode.Container, command: string[]) {
 
     const exec = await container.exec({
-      Cmd: ['code-server', '--install-extension', 'redhat.java'],
+      Cmd: command,
       AttachStderr: true,
       AttachStdout: true,
       AttachStdin: true,
@@ -175,12 +189,60 @@ export class ContainerService {
     });
 
     const stream = await exec.start({stdin: true});
-    stream?.pipe(process.stdout);
-    process.stdin.resume();
-    process.stdin.setEncoding("utf8");
-    process.stdin.setRawMode(true);
-    process.stdin.pipe(stream);
+    //output on console
+    //stream.pipe(process.stdout);
+    //container.modem.demuxStream(stream, process.stdout, process.stderr);
+    //stream.on('end', ()=> {});
+    await this.streamOnEndWorkaround(exec, stream);
+    return stream;
+  }
 
+
+  /* https://github.com/apocas/dockerode/issues/534
+  stream.on("end", resolve)
+  workaround */
+  private streamOnEndWorkaround(exec: Dockerode.Exec, stream: any): Promise<void>{
+    const finish = new Promise<void>((resolve) => {
+      // stream.on("end", resolve)
+      // workaround
+      const timer = setInterval(async () => {
+        const r = await exec.inspect();
+        if (!r.Running) {
+          clearInterval(timer);
+          stream.destroy();
+          resolve();
+        }
+      }, 1e3);
+    });
+    return finish;
+  }
+
+
+  private async installExtensions(container: Dockerode.Container, projectId: string) {
+    const bindPrefix = path.resolve(environment.docker.bindPrefix);
+    const extensionsListPath = `${bindPrefix}/config/${this.idBin(projectId)}/${projectId}/User/extensions.txt`;
+
+    let fileBuffer = await fs.promises.readFile(extensionsListPath).catch( async() => {
+      //create extensions.txt if not exists
+      await fs.promises.writeFile(extensionsListPath, "");
+    });
+
+    if(fileBuffer) {
+      let fileText = fileBuffer.toString('utf-8');
+      let array = fileText.split(/\r?\n/);
+      for(let i=0; i < array.length; ++i) {
+        array[i] = array[i].replace(/[^\w\d\s\\.\-]/g, '');
+        array[i] = array[i].trim();
+      }
+      // filter array for empty or undefined values
+      let cleanArr = array.filter(Boolean);
+      for(let i=0; i < cleanArr.length; ++i) {
+        //install
+        let stream = await this.containerExec(container,['code-server', '--install-extension', cleanArr[i]]);
+        //output to console
+        stream.pipe(process.stdout);
+      }
+    }
   }
 
 }
