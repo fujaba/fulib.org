d71914b71b39c5308ad87352ae59ba1195fff950
Adrian Kunz <clashsoft@hotmail.com>
2022-06-26 19:22:25 +0200

    feat(frontend): Configure custom Project image

--- frontend/src/app/projects/modules/projects-shared/project-form/project-form.component.ts
+++ frontend/src/app/projects/modules/projects-shared/project-form/project-form.component.ts
@@ -8,4 +8,12 @@ import {ProjectStub} from '../../../model/project';
 })
 export class ProjectFormComponent {
   @Input() project: ProjectStub;
+
+  dockerImages = [
+    {image: 'fulib/code-server-fulib', name: 'fulib', desc: 'code-server + JDK + Gradle + fulib dependencies (default)'},
+    {image: 'fulib/code-server-java', name: 'Java', desc: 'code-server + JDK + Gradle'},
+    {image: 'fulib/code-server-node', name: 'Node', desc: 'code-server + Node.js'},
+    {image: 'fulib/code-server-latex', name: 'LaTeX', desc: 'code-server + LaTeX'},
+    {image: 'fulib/code-server-python', name: 'Python', desc: 'code-server + Python'},
+  ];
 }
