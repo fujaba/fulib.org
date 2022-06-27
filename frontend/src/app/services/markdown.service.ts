562418c49a4e9f1d0704daef652b55083c531e2d
Adrian Kunz <clashsoft@hotmail.com>
2022-06-27 15:59:34 +0200

    refactor(frontend): Commonmark in frontend

--- frontend/src/app/services/markdown.service.ts
+++ frontend/src/app/services/markdown.service.ts
@@ -1,29 +1,37 @@
-import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
+import {HtmlRenderer, NodeWalkingStep, Parser} from 'commonmark';
 import {Observable} from 'rxjs';
-import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MarkdownService {
-  constructor(
-    private http: HttpClient,
-  ) {
-  }
+  private readonly parser = new Parser();
+
+  private renderer = new HtmlRenderer({
+    safe: true,
+  });
 
   renderMarkdown(md: string, options: { imageBaseUrl?: string; linkBaseUrl?: string; } = {}): Observable<string> {
-    const {imageBaseUrl, linkBaseUrl} = options;
-    const params: Record<string, string> = {};
-    if (imageBaseUrl) {
-      params.image_base_url = imageBaseUrl;
-    }
-    if (linkBaseUrl) {
-      params.link_base_url = linkBaseUrl;
-    }
-    return this.http.post(`${environment.apiURL}/rendermarkdown`, md, {
-      responseType: 'text',
-      params,
+    return new Observable(observer => {
+      observer.next(this.renderMarkdownSync(options, md));
     });
+  }
+
+  renderMarkdownSync(options: { imageBaseUrl?: string; linkBaseUrl?: string }, md: string): string {
+    const {imageBaseUrl, linkBaseUrl} = options;
+    const root = this.parser.parse(md);
+    const walker = root.walker();
+    let event: NodeWalkingStep;
+    while ((event = walker.next())) {
+      const node = event.node;
+      if (event.entering && node.type === 'link' && node.destination !== null && linkBaseUrl) {
+        node.destination = new URL(node.destination, linkBaseUrl).toString();
+      }
+      if (event.entering && node.type === 'image' && node.destination !== null && imageBaseUrl) {
+        node.destination = new URL(node.destination, imageBaseUrl).toString();
+      }
+    }
+    return this.renderer.render(root);
   }
 }
