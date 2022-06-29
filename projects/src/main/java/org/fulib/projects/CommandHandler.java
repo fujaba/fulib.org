8abf6478c9f74f7a7f1e9d21ea116d38f82ec6a7
Alexej Fromm <alexej.fromm@gmx.de>
2022-06-29 14:26:57 +0200

    add gradle to Dockerfile and some refactorings
    services/apps/projects/Dockerfile --> projects/Dockerfile
    -rename 'containerImage' --> 'fulib/fulib.org-code-server'
    -deleted files in /projects folder (not needed anymore)
    container.service.ts:
     -added createFile() helper method
     -added Machine settings bind

--- projects/src/main/java/org/fulib/projects/CommandHandler.java
+++
@@ -1,9 +0,0 @@
-package org.fulib.projects;
-
-import org.eclipse.jetty.websocket.api.Session;
-import org.json.JSONObject;
-
-public interface CommandHandler
-{
-	boolean handle(String command, String message, JSONObject json, Session session) throws Exception;
-}
