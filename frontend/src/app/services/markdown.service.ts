6f5e95b0b454883785a562f205e26b8ca587ec86
Adrian Kunz <clashsoft@hotmail.com>
2022-09-12 10:11:51 +0200

    refactor(frontend): Use markdown-it instead of commonmark

--- frontend/src/app/services/markdown.service.ts
+++ frontend/src/app/services/markdown.service.ts
@@ -1,37 +1,45 @@
 import {Injectable} from '@angular/core';
-import {HtmlRenderer, NodeWalkingStep, Parser} from 'commonmark';
+import MarkdownIt from 'markdown-it';
 import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MarkdownService {
-  private readonly parser = new Parser();
-
-  private renderer = new HtmlRenderer({
-    safe: true,
+  private readonly parser = new MarkdownIt({
+    linkify: true,
+    html: true,
   });
 
   renderMarkdown(md: string, options: { imageBaseUrl?: string; linkBaseUrl?: string; } = {}): Observable<string> {
-    return new Observable(observer => {
-      observer.next(this.renderMarkdownSync(options, md));
+    return new Observable(subscriber => {
+      subscriber.next(this.renderMarkdownSync(md, options));
+      subscriber.complete();
     });
   }
 
-  renderMarkdownSync(options: { imageBaseUrl?: string; linkBaseUrl?: string }, md: string): string {
+  renderMarkdownSync(md: string, options: { imageBaseUrl?: string; linkBaseUrl?: string }): string {
     const {imageBaseUrl, linkBaseUrl} = options;
-    const root = this.parser.parse(md);
-    const walker = root.walker();
-    let event: NodeWalkingStep;
-    while ((event = walker.next())) {
-      const node = event.node;
-      if (event.entering && node.type === 'link' && node.destination !== null && linkBaseUrl) {
-        node.destination = new URL(node.destination, linkBaseUrl).toString();
-      }
-      if (event.entering && node.type === 'image' && node.destination !== null && imageBaseUrl) {
-        node.destination = new URL(node.destination, imageBaseUrl).toString();
+    const tokens = this.parser.parse(md, {});
+    for (let token of tokens) {
+      if (token.type === 'inline') {
+        for (let child of (token.children || [])) {
+          switch (child.type) {
+            case 'link_open':
+              const href = child.attrGet('href');
+              linkBaseUrl && href && child.attrSet('href', linkBaseUrl + href);
+              break;
+            case 'image':
+              const src = child.attrGet('src');
+              imageBaseUrl && src && child.attrSet('src', imageBaseUrl + src);
+              break;
+          }
+        }
+      } else if (token.type === 'table_open') {
+        token.attrSet('class', 'table table-bordered');
       }
     }
-    return this.renderer.render(root);
+    const html = this.parser.renderer.render(tokens, {}, {});
+    return html;
   }
 }
