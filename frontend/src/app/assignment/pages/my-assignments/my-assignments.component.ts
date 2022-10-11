a75d5e3738611d1a04a1c18e043cce6a09fb08ea
Adrian Kunz <clashsoft@hotmail.com>
2022-10-11 16:45:32 +0200

    feat(frontend): Archive/unarchive assignments action

--- frontend/src/app/assignment/pages/my-assignments/my-assignments.component.ts
+++ frontend/src/app/assignment/pages/my-assignments/my-assignments.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {ToastService} from 'ng-bootstrap-ext';
 import {map, switchMap, tap} from 'rxjs/operators';
 import Assignment from '../../model/assignment';
 import {AssignmentService} from '../../services/assignment.service';
@@ -16,6 +17,7 @@ export class MyAssignmentsComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private assignmentService: AssignmentService,
+    private toastService: ToastService,
   ) {
   }
 
@@ -25,5 +27,16 @@ export class MyAssignmentsComponent implements OnInit {
       tap(archived => this.archived = archived),
       switchMap(archived => this.assignmentService.getOwn(archived)),
     ).subscribe(assignments => this.assignments = assignments);
+  }
+
+  archive({_id, archived}: Assignment) {
+    this.assignmentService.update(_id, {
+      archived: !archived,
+    }).subscribe(() => {
+      this.assignments = this.assignments?.filter(a => a._id !== _id);
+      this.toastService.warn('Archive Assignment', `Successfully ${archived ? 'un' : ''}archived assignment`);
+    }, error => {
+      this.toastService.error('Archive Assignment', `Failed to ${archived ? 'un' : ''}archive assignment`, error);
+    });
   }
 }
