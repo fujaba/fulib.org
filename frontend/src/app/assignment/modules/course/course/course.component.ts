86ab5d878d3188e76d1011ec9b7d684583d41a8c
Adrian Kunz <clashsoft@hotmail.com>
2022-11-09 15:21:59 +0100

    fix(frontend): Show course title and description in overview

--- frontend/src/app/assignment/modules/course/course/course.component.ts
+++ frontend/src/app/assignment/modules/course/course/course.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {switchMap} from 'rxjs';
+import Course from '../../../model/course';
+import {CourseService} from '../../../services/course.service';
 import {courseChildren} from '../course-routing.module';
 
 @Component({
@@ -7,12 +11,20 @@ import {courseChildren} from '../course-routing.module';
   styleUrls: ['./course.component.scss'],
 })
 export class CourseComponent implements OnInit {
-  routes = courseChildren;
+  course?: Course;
 
-  constructor() {
+  readonly routes = courseChildren;
+
+  constructor(
+    private route: ActivatedRoute,
+    private courseService: CourseService,
+  ) {
   }
 
   ngOnInit(): void {
+    this.route.params.pipe(
+      switchMap(({cid}) => this.courseService.get(cid)),
+    ).subscribe(course => this.course = course);
   }
 
 }
