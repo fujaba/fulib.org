7c65a97cb2ab232425993456dea9fcd64e560ef0
Adrian Kunz <clashsoft@hotmail.com>
2022-12-05 16:16:39 +0100

    dev(frontend): Standalone configuration

--- frontend/src/environments/environment.standalone.ts
+++ frontend/src/environments/environment.standalone.ts
@@ -1,15 +1,13 @@
 export const environment = {
-  production: false,
+  production: true,
   apiURL: 'http://localhost:4567/api',
   assignmentsApiUrl: 'http://localhost:21318/api/v1',
   projectsApiUrl: 'http://localhost:6266/api/v1',
   projectsProxyUrl: 'http://localhost:13147',
   workflowsUrl: 'http://localhost:11356/api/v1',
-  auth: {
-    url: 'https://se.uniks.de/auth',
-    realm: 'fulib.org',
-    clientId: 'fulib.org',
-  },
+  auth: null as {
+    url: string;
+    realm: string;
+    clientId: string;
+  } | null,
 };
-
-import 'zone.js/plugins/zone-error';
