23e4fd5cd4ad79af1da05c6fdf572aae66cc21f0
Adrian Kunz <clashsoft@hotmail.com>
2023-07-14 20:22:28 +0200

    fix(assignments-service): Module import

--- services/apps/assignments/src/embedding/embedding.module.ts
+++ services/apps/assignments/src/embedding/embedding.module.ts
@@ -1,18 +1,21 @@
-import { Module } from '@nestjs/common';
-import { EmbeddingService } from './embedding.service';
-import { EmbeddingController } from './embedding.controller';
+import {Module} from '@nestjs/common';
+import {EmbeddingService} from './embedding.service';
+import {EmbeddingController} from './embedding.controller';
 import {SearchModule} from "../search/search.module";
 import {ClassroomModule} from "../classroom/classroom.module";
 import {ElasticsearchModule} from "@nestjs/elasticsearch";
 import {environment} from "../environment";
+import {AssignmentModule} from "../assignment/assignment.module";
 
 @Module({
   imports: [
     ElasticsearchModule.register(environment.elasticsearch), // TODO: move to search module
     SearchModule,
     ClassroomModule,
+    AssignmentModule,
   ],
   providers: [EmbeddingService],
   controllers: [EmbeddingController]
 })
-export class EmbeddingModule {}
+export class EmbeddingModule {
+}
