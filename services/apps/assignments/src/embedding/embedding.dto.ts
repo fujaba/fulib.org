61334324ff5b05d3d0e37afc089f23ffd0deb8e4
Adrian Kunz <a.kunz@uni-kassel.de>
2023-09-12 11:11:42 +0200

    style(assignments-service): Remove unnecessary import

--- services/apps/assignments/src/embedding/embedding.dto.ts
+++ services/apps/assignments/src/embedding/embedding.dto.ts
@@ -1,4 +1,4 @@
-import {ApiProperty, PickType} from "@nestjs/swagger";
+import {ApiProperty} from "@nestjs/swagger";
 import {ArrayMaxSize, ArrayMinSize, IsIn, IsInt, IsMongoId, IsNumber, IsString} from "class-validator";
 
 export class EmbeddingEstimate {
