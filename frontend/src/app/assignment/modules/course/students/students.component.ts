88ed967ee5eb513f218178a37f7df19ed2744713
Adrian Kunz <a.kunz@uni-kassel.de>
2023-09-15 13:42:27 +0200

    fix(frontend): Better column names in course students table from common prefix

--- frontend/src/app/assignment/modules/course/students/students.component.ts
+++ frontend/src/app/assignment/modules/course/students/students.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {switchMap} from 'rxjs/operators';
+import {switchMap, tap} from 'rxjs/operators';
 import {CourseService} from 'src/app/assignment/services/course.service';
 import Course, {CourseStudent} from '../../../model/course';
 import {authorInfoProperties} from '../../../model/solution';
+import {AssignmentService} from "../../../services/assignment.service";
 
 @Component({
   selector: 'app-students',
@@ -13,6 +14,7 @@ import {authorInfoProperties} from '../../../model/solution';
 export class StudentsComponent implements OnInit {
   course?: Course;
   students: CourseStudent[] = [];
+  assignmentNames: string[] = [];
   assignees: string[] = [];
 
   readonly authorProperties = authorInfoProperties;
@@ -20,13 +22,30 @@ export class StudentsComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private courseService: CourseService,
+    private assignmentService: AssignmentService,
   ) {
   }
 
   ngOnInit(): void {
     this.route.params.pipe(
       switchMap(({cid}) => this.courseService.get(cid)),
-    ).subscribe(course => this.course = course);
+      tap(course => this.course = course),
+      switchMap(course => this.assignmentService.findAll(course.assignments)),
+    ).subscribe(assignments => {
+      if (!assignments.length) {
+        return;
+      }
+      const firstTitle = assignments[0].title;
+      if (assignments.length === 1) {
+        this.assignmentNames = [firstTitle];
+        return;
+      }
+      let prefixLength = 0;
+      while (prefixLength < firstTitle.length && assignments.every(a => a.title[prefixLength] === firstTitle[prefixLength])) {
+        prefixLength++;
+      }
+      this.assignmentNames = assignments.map(a => a.title.slice(prefixLength));
+    });
 
     this.route.params.pipe(
       switchMap(({cid}) => this.courseService.getStudents(cid)),
