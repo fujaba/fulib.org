f332e0e8cf382d9d2844cae9a593c0c1c387a7df
Adrian Kunz <a.kunz@uni-kassel.de>
2023-09-27 14:15:14 +0200

    feat(issue-reactions): Environment, Sentry and MongoDB

--- services/apps/issue-reactions/src/issue-reactions.module.ts
+++ services/apps/issue-reactions/src/issue-reactions.module.ts
@@ -1,10 +1,37 @@
-import {Module} from '@nestjs/common';
+import {HttpException, Module} from '@nestjs/common';
 import {IssueReactionsService} from './issue-reactions.service';
+import {MongooseModule} from "@nestjs/mongoose";
+import {environment} from "../../assignments/src/environment";
+import {SentryInterceptor, SentryModule} from "@ntegral/nestjs-sentry";
+import {APP_INTERCEPTOR} from "@nestjs/core";
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(environment.mongo.uri, environment.mongo.options),
+    SentryModule.forRoot({
+      dsn: environment.sentryDsn,
+      environment: environment.nodeEnv,
+      release: environment.version,
+      initialScope: {
+        tags: {
+          service: 'issue-reactions',
+        },
+      },
+    }),
+  ],
   controllers: [],
-  providers: [IssueReactionsService],
+  providers: [
+    IssueReactionsService,
+    {
+      provide: APP_INTERCEPTOR,
+      useFactory: () => new SentryInterceptor({
+        filters: [{
+          type: HttpException,
+          filter: (exception: HttpException) => 500 > exception.getStatus(),
+        }],
+      }),
+    },
+  ],
 })
 export class IssueReactionsModule {
 }
--- services/apps/issue-reactions/src/main.ts
+++ services/apps/issue-reactions/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { IssueReactionsModule } from './issue-reactions.module';
+import {environment} from "./environment";
 
 async function bootstrap() {
   const app = await NestFactory.create(IssueReactionsModule);
-  await app.listen(3000);
+  await app.listen(environment.port);
 }
 bootstrap();
