de0ed8816b4cfcdcdcb635e2e80081ed5a398b10
Adrian Kunz <a.kunz@uni-kassel.de>
2023-11-07 11:09:09 +0100

    feat(frontend): Clickable assignments in course students header

--- frontend/src/app/assignment/modules/course/students/students.component.ts
+++ frontend/src/app/assignment/modules/course/students/students.component.ts
@@ -5,6 +5,7 @@ import {CourseService} from 'src/app/assignment/services/course.service';
 import Course, {CourseStudent} from '../../../model/course';
 import {authorInfoProperties} from '../../../model/solution';
 import {AssignmentService} from "../../../services/assignment.service";
+import Assignment, {ReadAssignmentDto} from "../../../model/assignment";
 
 @Component({
   selector: 'app-students',
@@ -14,6 +15,7 @@ import {AssignmentService} from "../../../services/assignment.service";
 export class StudentsComponent implements OnInit {
   course?: Course;
   students?: CourseStudent[];
+  assignments: (ReadAssignmentDto | undefined)[] = [];
   assignmentNames: string[] = [];
   assignees: string[] = [];
 
@@ -32,6 +34,7 @@ export class StudentsComponent implements OnInit {
       tap(course => this.course = course),
       switchMap(course => this.assignmentService.findIds(course.assignments)),
     ).subscribe(assignments => {
+      this.assignments = assignments;
       if (!assignments.length) {
         return;
       }
