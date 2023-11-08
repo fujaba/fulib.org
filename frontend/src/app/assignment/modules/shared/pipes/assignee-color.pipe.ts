75affecd15e4a511f6dbbb3f2bb7d367b92715ae
Adrian Kunz <a.kunz@uni-kassel.de>
2023-11-08 14:01:42 +0100

    fix(frontend): Better assignee color

--- frontend/src/app/assignment/modules/shared/pipes/assignee-color.pipe.ts
+++ frontend/src/app/assignment/modules/shared/pipes/assignee-color.pipe.ts
@@ -9,8 +9,17 @@ export class AssigneeColorPipe implements PipeTransform {
     if (!value) {
       return undefined;
     }
-    const hash = value.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0);
+    const hash = hashCode(value);
     const hue = (hash % 30) * 12;
     return `hsl(${hue}, 50%, 50%)`;
   }
+}
+
+function hashCode(str: string): number {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = ((hash << 5) - hash) + str.charCodeAt(i);
+    hash |= 0;
+  }
+  return hash;
 }
