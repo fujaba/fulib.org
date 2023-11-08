27aa06986b031458975c64bb6ed4135d2ed7779e
Adrian Kunz <a.kunz@uni-kassel.de>
2023-11-08 12:49:12 +0100

    refactor(frontend): Simplify solution table copy methods

--- frontend/src/app/assignment/modules/assignment/solution-table/solution-table.component.ts
+++ frontend/src/app/assignment/modules/assignment/solution-table/solution-table.component.ts
@@ -191,17 +191,19 @@ export class SolutionTableComponent implements OnInit {
   }
 
   copyPoints() {
-    this.clipboardService.copy(this.solutions!.map(s => s.points ?? '').join('\n'));
-    this.toastService.success('Copy Points', `Copied ${this.solutions.length} rows to clipboard`);
+    this.copy('Points', s => (s.points || '').toString());
   }
 
   copyAssignee() {
-    this.clipboardService.copy(this.solutions!.map(s => this.assignees[s._id!] || '').join('\n'));
-    this.toastService.success('Copy Assignees', `Copied ${this.solutions.length} rows to clipboard`);
+    this.copy('Assignees', s => this.assignees[s._id!] || '');
   }
 
   copyAuthor(name: string, key: keyof AuthorInfo) {
-    this.clipboardService.copy(this.solutions!.map(s => s.author[key] ?? '').join('\n'));
+    this.copy(name, s => s.author[key] || '');
+  }
+
+  copy(name: string, select: (s: Solution) => string) {
+    this.clipboardService.copy(this.solutions!.map(select).join('\n'));
     this.toastService.success(`Copy ${name}`, `Copied ${this.solutions.length} rows to clipboard`);
   }
 
