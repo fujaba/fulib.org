082921d6245f46e5c06e11251f54e6b973562cd1
Adrian Kunz <a.kunz@uni-kassel.de>
2023-12-11 14:49:12 +0100

    feat(frontend): View differences in Preview before saving assignment

--- frontend/src/app/assignment/modules/edit-assignment/preview/preview.component.ts
+++ frontend/src/app/assignment/modules/edit-assignment/preview/preview.component.ts
@@ -1,8 +1,23 @@
 import {Component, OnInit} from '@angular/core';
 import {KeycloakService} from 'keycloak-angular';
 import {CreateAssignmentDto} from '../../../model/assignment';
-import Task from '../../../model/task';
 import {AssignmentContext} from '../../../services/assignment.context';
+import * as jsdiff from 'diff';
+import {AssignmentService} from "../../../services/assignment.service";
+import {TaskMarkdownService} from "../task-markdown.service";
+
+function escapeHtml(text: string) {
+  return text.replace(/[&<>"'`=\/]/g, function (s) {
+    return `&#${s.charCodeAt(0)};`;
+  });
+}
+
+function renderDiff(diff: jsdiff.Change[]) {
+  return diff.map(part => {
+    const color = part.added ? 'text-success' : part.removed ? 'text-danger' : '';
+    return `<span class="${color}">${escapeHtml(part.value)}</span>`;
+  }).join('');
+}
 
 @Component({
   selector: 'app-edit-assignment-preview',
@@ -11,20 +26,33 @@ import {AssignmentContext} from '../../../services/assignment.context';
 })
 export class PreviewComponent implements OnInit {
   assignment: CreateAssignmentDto;
-  tasks: Task[];
+  diffMetadata?: string;
+  diffTasks?: string;
 
   loggedIn = false;
 
   constructor(
     private keycloakService: KeycloakService,
     context: AssignmentContext,
+    private assignmentService: AssignmentService,
+    private taskMarkdownService: TaskMarkdownService,
   ) {
-    this.assignment = context.assignment;
-    this.tasks = this.assignment.tasks.filter(t => !t.deleted);
+    this.assignment = context.getAssignment();
   }
 
   ngOnInit(): void {
     this.loggedIn = this.keycloakService.isLoggedIn();
+
+    if ('_id' in this.assignment) {
+      this.assignmentService.get(this.assignment._id as string).subscribe(oldAssignment => {
+        const {tasks: oldTasks, ...old} = oldAssignment;
+        const {tasks: newTasks, ...current} = this.assignment;
+        this.diffMetadata = renderDiff(jsdiff.diffJson(old, current));
+        const oldTasksMd = this.taskMarkdownService.renderTasks(oldTasks);
+        const newTasksMd = this.taskMarkdownService.renderTasks(newTasks);
+        this.diffTasks = renderDiff(jsdiff.diffWords(oldTasksMd, newTasksMd));
+      });
+    }
   }
 
   login(): void {
--- frontend/src/app/assignment/services/assignment.context.ts
+++ frontend/src/app/assignment/services/assignment.context.ts
@@ -1,8 +1,23 @@
 import {Injectable} from '@angular/core';
 import Assignment, {CreateAssignmentDto} from '../model/assignment';
+import Task from "../model/task";
 
 @Injectable()
 export class AssignmentContext {
   assignment: Assignment | CreateAssignmentDto;
   saveDraft: () => void;
+
+  getAssignment(): Assignment | CreateAssignmentDto {
+    return {
+      ...this.assignment,
+      tasks: this.getTasks(this.assignment.tasks),
+    };
+  }
+
+  private getTasks(tasks: Task[]): Task[] {
+    return tasks.filter(t => !t.deleted).map(({deleted, collapsed, children, ...rest}) => ({
+      ...rest,
+      children: this.getTasks(children),
+    }));
+  }
 }
