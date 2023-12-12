8a02c3e47dfd2f9f762305b13d46cad8ae8b3bd7
Adrian Kunz <a.kunz@uni-kassel.de>
2023-12-12 14:18:41 +0100

    fix(projects-service): Use MongooseRepository and EventRepository for Projects

--- services/apps/projects/src/project/project.service.ts
+++ services/apps/projects/src/project/project.service.ts
@@ -1,39 +1,22 @@
 import {UserToken} from '@app/keycloak-auth';
 import {Injectable} from '@nestjs/common';
 import {InjectModel} from '@nestjs/mongoose';
-import {FilterQuery, Model, Types} from 'mongoose';
-import {CreateProjectDto, UpdateProjectDto} from './project.dto';
+import {Model} from 'mongoose';
 import {Project, ProjectDocument} from './project.schema';
+import {EventRepository, EventService, MongooseRepository} from "@mean-stream/nestx";
 
 @Injectable()
-export class ProjectService {
+@EventRepository()
+export class ProjectService extends MongooseRepository<Project> {
   constructor(
-    @InjectModel(Project.name) private model: Model<Project>,
+    @InjectModel(Project.name) model: Model<Project>,
+    private eventService: EventService,
   ) {
+    super(model);
   }
 
-  async create(dto: CreateProjectDto, userId: string): Promise<ProjectDocument> {
-    return this.model.create({
-      ...dto,
-      userId,
-      created: new Date(),
-    });
-  }
-
-  async findAll(where: FilterQuery<Project> = {}): Promise<Project[]> {
-    return this.model.find(where).sort('+name').exec();
-  }
-
-  async findOne(id: Types.ObjectId): Promise<ProjectDocument | null> {
-    return this.model.findById(id).exec();
-  }
-
-  async update(id: Types.ObjectId, dto: UpdateProjectDto): Promise<Project | null> {
-    return this.model.findByIdAndUpdate(id, dto, {new: true}).exec();
-  }
-
-  async remove(id: Types.ObjectId): Promise<ProjectDocument | null> {
-    return this.model.findByIdAndDelete(id).exec();
+  emit(event: string, project: ProjectDocument) {
+    this.eventService.emit(`projects.${project._id}.${event}`, project);
   }
 
   isAuthorized(project: Project, user: UserToken) {
