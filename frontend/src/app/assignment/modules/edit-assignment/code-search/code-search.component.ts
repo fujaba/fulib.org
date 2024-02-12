bbe1aa5a486e939e05966657fa826dfc490228e6
Adrian Kunz <a.kunz@uni-kassel.de>
2024-02-12 13:13:13 +0100

    feat(frontend): Customizable embedding model

--- frontend/src/app/assignment/modules/edit-assignment/code-search/code-search.component.ts
+++ frontend/src/app/assignment/modules/edit-assignment/code-search/code-search.component.ts
@@ -9,11 +9,17 @@ import {ClassroomInfo} from "../../../model/assignment";
 })
 export class CodeSearchComponent {
   classroom: ClassroomInfo;
+  embeddingModels = [
+    {id: 'text-embedding-3-small', label: 'Cheapest', labelBg: 'success'},
+    {id: 'text-embedding-3-large', label: 'Most accurate', labelBg: 'primary'},
+    {id: 'text-embedding-ada-002', label: 'Legacy', labelBg: 'secondary'},
+  ] as const;
 
   constructor(
     readonly context: AssignmentContext,
   ) {
     this.classroom = this.context.assignment.classroom ||= {};
     this.classroom.openaiConsent ??= true;
+    this.classroom.openaiModel ??= 'text-embedding-ada-002';
   }
 }
