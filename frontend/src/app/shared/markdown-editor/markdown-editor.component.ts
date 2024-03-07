28247594e86accab2bce85faffa4de315f766ede
Adrian Kunz <a.kunz@uni-kassel.de>
2024-03-07 11:19:22 +0100

    feat(frontend): Quick action buttons in markdown editor

--- frontend/src/app/shared/markdown-editor/markdown-editor.component.ts
+++ frontend/src/app/shared/markdown-editor/markdown-editor.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-markdown-editor',
@@ -17,5 +17,31 @@ export class MarkdownEditorComponent {
   /** Ctrl-Enter */
   @Output() submit = new EventEmitter<string>();
 
+  @ViewChild('textarea', {static: false}) textarea?: ElementRef<HTMLTextAreaElement>;
+
   preview = false;
+
+  insertText(before: string, after: string) {
+    if (!this.textarea) {
+      return;
+    }
+    const textarea = this.textarea.nativeElement;
+
+    const selection = textarea.value.substring(textarea.selectionStart, textarea.selectionEnd);
+    const extendedSelection = textarea.value.substring(textarea.selectionStart - before.length, textarea.selectionEnd + after.length);
+    if (selection.startsWith(before) && selection.endsWith(after)) {
+      // Remove the before and after text from the selection
+      textarea.setRangeText(selection.substring(before.length, selection.length - after.length), textarea.selectionStart, textarea.selectionEnd, 'select');
+    } else if (extendedSelection.startsWith(before) && extendedSelection.endsWith(after)) {
+      // Remove the before and after text from the extended selection
+      textarea.setRangeText(extendedSelection.substring(before.length, extendedSelection.length - after.length), textarea.selectionStart - before.length, textarea.selectionEnd + after.length, 'select');
+    } else {
+      // Insert the before and after text around the selection
+      textarea.setRangeText(before + selection + after, textarea.selectionStart, textarea.selectionEnd, 'select');
+    }
+
+    this.content = textarea.value;
+    this.contentChange.emit(textarea.value);
+    textarea.focus();
+  }
 }
