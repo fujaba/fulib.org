76ed0e76fa5477e8100b7660efe01fea8ef9e37e
Adrian Kunz <a.kunz@uni-kassel.de>
2024-09-30 13:37:17 +0200

    chore(frontend): Add @angular/localize/init to polyfills

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -15,11 +15,6 @@
  */
 
 /***************************************************************************************************
- * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
- */
-import '@angular/localize/init';
-
-/***************************************************************************************************
  * BROWSER POLYFILLS
  */
 /**
