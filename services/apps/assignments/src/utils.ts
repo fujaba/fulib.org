622c6d3d816ba1030a296f1edb6fbe896bf6bf6a
Adrian Kunz <a.kunz@uni-kassel.de>
2024-10-01 12:43:36 +0200

    fix(services): Lint problems

--- services/apps/assignments/src/utils.ts
+++ services/apps/assignments/src/utils.ts
@@ -1,12 +1,12 @@
 import {MessageEvent} from '@nestjs/common';
 import {WsResponse} from '@nestjs/websockets';
 import {randomBytes} from 'crypto';
-import {interval, map, mapTo, merge, Observable, of} from 'rxjs';
+import {interval, map, mapTo, merge, Observable} from 'rxjs';
 
 export function generateToken(): string {
   const bytes = randomBytes(8);
   const hex = bytes.toString('hex');
-  return new Array(4).fill(0).map((_, index) => hex.substr(index * 4, 4)).join('-');
+  return new Array(4).fill(0).map((_, index) => hex.substring(index * 4, 4)).join('-');
 }
 
 export function eventStream<T>(source: Observable<WsResponse<T>>, name: string): Observable<MessageEvent> {
