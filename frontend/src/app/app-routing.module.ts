2e006c1a29e87116e34a204515bdfe069d152186
Adrian Kunz <a.kunz@uni-kassel.de>
2025-04-27 14:32:29 +0200

    remove(frontend): Feedback dialog

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import {RouterModule, Routes} from '@angular/router';
 
 import {AboutComponent} from './components/about/about.component';
 import {ChangelogComponent} from './components/changelog/changelog.component';
-import {FeedbackComponent} from './components/feedback/feedback.component';
 import {HomeComponent} from './components/home/home.component';
 import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
 import {PrivacyComponent} from './components/privacy/privacy.component';
@@ -14,7 +13,6 @@ const routes: Routes = [
   {path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule)},
   {path: '', pathMatch: 'full', component: HomeComponent, data: {title: 'Home'}},
   {path: '**', component: PageNotFoundComponent, data: {title: '404'}},
-  {outlet: 'modal', path: 'feedback', component: FeedbackComponent, data: {title: 'Feedback'}},
   {outlet: 'modal', path: 'privacy', component: PrivacyComponent, data: {title: 'Privacy'}},
   {outlet: 'modal', path: 'changelog', component: ChangelogComponent, data: {title: 'Changelog'}},
   {outlet: 'modal', path: 'about', component: AboutComponent, data: {title: 'About'}},
