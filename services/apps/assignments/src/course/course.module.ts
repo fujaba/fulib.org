4c47dacc2abcadf7cde7b9934e8b98ce457293eb
Adrian Kunz <a.kunz@uni-kassel.de>
2025-04-28 17:57:45 +0200

    fix(assignments-service): Wrong module import

--- services/apps/assignments/src/course/course.module.ts
+++ services/apps/assignments/src/course/course.module.ts
@@ -1,11 +1,11 @@
-import {Module} from '@nestjs/common';
+import {forwardRef, Module} from '@nestjs/common';
 import {MongooseModule} from '@nestjs/mongoose';
 import {AssigneeModule} from '../assignee/assignee.module';
+import {CourseMemberModule} from '../course-member/course-member.module';
 import {SolutionModule} from '../solution/solution.module';
 import {CourseController} from './course.controller';
 import {Course, CourseSchema} from './course.schema';
 import {CourseService} from './course.service';
-import {CourseMemberModule} from "../course-member/course-member.module";
 
 @Module({
   imports: [
@@ -17,7 +17,7 @@ import {CourseMemberModule} from "../course-member/course-member.module";
     ]),
     SolutionModule,
     AssigneeModule,
-    CourseMemberModule,
+    forwardRef(() => CourseMemberModule),
   ],
   controllers: [CourseController],
   providers: [
