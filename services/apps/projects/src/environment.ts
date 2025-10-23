1358314f6cb0c54bf8436aa9de671e1b472893af
Adrian Kunz <a.kunz@uni-kassel.de>
2025-10-23 13:32:53 +0200

    fix(services): Remove se.uniks.de auth issuer

--- services/apps/projects/src/environment.ts
+++ services/apps/projects/src/environment.ts
@@ -20,14 +20,13 @@ export const environment = {
     heartbeatTimeout: +(process.env.FULIB_PROJECTS_HEARTBEAT_TIMEOUT || 15) * 60,
   },
   auth: {
-    url: 'https://se.uniks.de/auth',
     realm: 'fulib.org',
     publicKey: `-----BEGIN PUBLIC KEY-----\n${
       process.env.AUTH_PUBLIC_KEY || 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjSudUTRH8wtEeIQdKh4Yv97HcFGjLzl9+yBm+SO2lf2r/jvpbSRPWqMDbrgQ0BGDwMQLweUoPUzOhZbrScs5edhRxtn3nPKUTiBtOoFiYupTTicdIKY1FAlagSM4/7WBbj0CT14cXQoyZK2VbfYXsu19tdJ/oFPBAGwrsj/ugiGE6c8H6wu4yHPsJFDR8sN524NN4diXOKyZ731IP5lqfN4+bP17/b1KaDG8swsRNW93dfEz0nfcJu+wutI3dLc98/cekOpHUZRzndsshifjuM39RF6oyI89tcuwrrKqrz5HV9CsPBUB2hakGBhg71svZyq304ikQFYej/ydZd0biwIDAQAB'
     }\n-----END PUBLIC KEY-----`,
     resource: process.env.AUTH_RESOURCE || 'projects-service',
     algorithms: (process.env.AUTH_ALGORITHMS || 'RS256').split(','),
-    issuer: process.env.AUTH_ISSUER || 'https://se.uniks.de/auth/realms/fulib.org',
+    issuer: process.env.AUTH_ISSUER || 'https://fulib.uni-kassel.dev/auth/realms/fulib.org',
   },
   nats: {
     servers: process.env.NATS_URL || 'nats://localhost:4222',
